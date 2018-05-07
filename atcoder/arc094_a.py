9898c9fa0c1a04b5366d9ab921342d150b0f1915
daleksprinter <b1015120@fun.ac.jp>
2018-05-07 16:57:29 +0900

    arc094_a.py

---
+++ atcoder/arc094_a.py
@@ -0,0 +1,37 @@
+def count(line):
+	count = 0
+	if (line[0]+line[1]+line[2])%2 == 0:
+		flag = False
+		for i in range(3) :
+			if line[i]%2 == 1 :
+				line[i] += 1
+				flag = True
+
+		if flag == True:
+			count += 1
+		
+		max = 0
+		
+		for i in range(3):
+			if max < line[i]:
+				max = line[i]
+		for i in range(3):
+			count += (max - line[i])/2
+		return count
+	else :
+		flag = False
+		for i in range(3):
+			if line[i]%2 == 0:
+				line[i] += 1
+				flag = True
+		if flag == True:
+			count+= 1
+		max = 0
+		for i in range(3):
+			if max<line[i]:
+				max =line[i]
+		for i in range(3) : 
+			count += (max-line[i])/2
+		return count
+
+print count(map(int,raw_input().split()))
