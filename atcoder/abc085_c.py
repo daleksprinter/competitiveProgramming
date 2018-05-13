8003fa697b7708b11902266650480baa0dfb40f7
daleksprinter <b1015120@fun.ac.jp>
2018-05-13 19:40:04 +0900

    abc085_c.py

---
+++ atcoder/abc085_c.py
@@ -0,0 +1,20 @@
+num,yen = map(int,raw_input().split())
+
+ans = [-1,-1,-1]
+isanswered = False
+
+if 10000*num >= yen: 
+	for i in range(num+1):
+		for j in range(num+1-i):
+			if 10000*i + 5000*j + 1000*(num-i-j) == yen and num-i-j-(num-i-j) == 0:
+				ans[0] = i
+				ans[1] = j
+				ans[2] = (num-i-j)
+				isanswered = True
+				break
+
+		if isanswered == True :
+			break
+
+print '%d %d %d' %(ans[0],ans[1],ans[2])
+
