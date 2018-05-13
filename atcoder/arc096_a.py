a2679eae3d5a1aac0c34b0083bd85f8e6eb22f23
daleksprinter <b1015120@fun.ac.jp>
2018-05-13 20:34:22 +0900

    arc096_a.py

---
+++ atcoder/arc096_a.py
@@ -0,0 +1,18 @@
+a,b,c,x,y = map(int,raw_input().split())
+
+money = 0
+if a + b > c * 2 :
+
+	c_num = y * 2 if x > y else x * 2 #x > y ? y*2 : x*2
+	money += c * c_num
+
+	rest_money = a if x > y else b #x > y ? a : b
+	rest_num = x-y if x > y else y-x #x > y ? x-y : y-x
+	
+	money += c * 2 * rest_num if rest_money > c*2 else rest_money * rest_num
+
+else :
+	money += a * x + b * y
+
+print money 
+
