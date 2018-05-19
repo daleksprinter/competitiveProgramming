ac16668250a3826212992f3d7f43daac42301ec6
daleksprinter <b1015120@fun.ac.jp>
2018-05-19 21:29:56 +0900

    abc004_2.py

---
+++ atcoder/abc004_2.py
@@ -0,0 +1,15 @@
+arr = [[] for i in range(4)]
+ans = [['' for j in range(4)] for i in range(4)]
+
+for i in range(4):
+	arr[i] = raw_input().split()
+
+
+for i in range(4):
+	for j in range(4):
+		ans[i][j] = arr[3-i][3-j]
+
+for i in range(4):
+	print '%s %s %s %s' %(ans[i][0],ans[i][1],ans[i][2],ans[i][3])
+
+
