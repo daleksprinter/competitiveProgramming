5bbb4fc111936e9cfe453fd45f3a73bfb80ee66d
daleksprinter <b1015120@fun.ac.jp>
2018-06-01 23:25:30 +0900

    agc002_b.py

---
+++ atcoder/agc002_b.py
@@ -0,0 +1,30 @@
+n,m = map(int,raw_input().split())
+
+arr = [1 for i in range(n)]
+isexist = [False for i in range(n)]
+
+isexist[0] = True
+
+for i in range(m):
+	frm,to = map(int,raw_input().split())
+	frm -= 1
+	to -= 1
+
+	arr[frm] -= 1
+	arr[to] += 1
+
+	if isexist[frm] :
+		isexist[to] = True
+
+	if arr[frm] == 0:
+		isexist[frm] = False
+
+
+
+
+count = 0
+for i in range(n):
+	if isexist[i] : 
+		count += 1
+
+print count
