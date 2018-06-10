a758622be97760ffc72a459ba3278a715e86dd41
daleksprinter <b1015120@fun.ac.jp>
2018-06-11 00:25:19 +0900

    arc098_a.py

---
+++ atcoder/arc098_a.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+s = list(raw_input())
+
+cs = []
+for i in range(n):
+	if s[i] == 'E':
+		s[i] = 1
+	else :
+		s[i] = 0
+
+cs.append(s[0])
+
+for i in range(1,n):
+	cs.append(cs[i-1]+s[i])
+
+ans = cs[n-1]-cs[0]
+for i in range(1,n):
+	ans = min( cs[n-1]-cs[i] + i-cs[i-1],ans)
+
+print ans
