b3b512808e939179a7b25cf83a0135339f0b88ef
daleksprinter <b1015120@fun.ac.jp>
2018-06-11 15:36:06 +0900

    abc006_3.py

---
+++ atcoder/abc006_3.py
@@ -0,0 +1,15 @@
+n,m = map(int,raw_input().split())
+
+isans = False
+for i in range(n+1):
+	if 2*i + 4*(n-i-1) == m-3 and n-i-1 >= 0:
+		print '%d %d %d' %(i,1,n-i-1)
+		isans = True
+		break
+	elif 2*i + 4*(n-i) == m :
+		print '%d %d %d' %(i,0,n-i)
+		isans = True
+		break
+
+if not isans:
+	print '-1 -1 -1'
