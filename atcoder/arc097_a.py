add22cde9287d5c9f6c8ed5ae609e7823e6e2fc6
daleksprinter <b1015120@fun.ac.jp>
2018-06-15 01:11:24 +0900

    arc097_a.py

---
+++ atcoder/arc097_a.py
@@ -0,0 +1,14 @@
+s = raw_input()
+k = int(input())
+l = len(s)
+arr = []
+
+for i in range(l):
+	for j in range(1,min(6,l-i+1)):
+		arr.append(s[i:i+j])
+
+arr_uni = list(set(arr))
+arr_uni.sort()
+print arr_uni[k-1]
+
+
