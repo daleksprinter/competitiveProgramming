4b67abbf1a89daa33aecf710154a13a5309e9970
daleksprinter <b1015120@fun.ac.jp>
2018-06-15 19:19:09 +0900

    abc079_c.py

---
+++ atcoder/abc079_c.py
@@ -0,0 +1,17 @@
+arr = map(int,list(raw_input()))
+
+printed = False
+
+def comb(strs,sums,i):
+		global printed
+		if i == 3 and sums == 7 and not printed: 
+			print str(arr[0]) + strs[0] + str(arr[1]) + strs[1] + str(arr[2]) + strs[2] + str(arr[3]) + "=7"
+			printed = True
+
+		elif i < 3:
+			comb(strs+'+',sums+arr[i+1],i+1)
+			comb(strs+'-',sums-arr[i+1],i+1)
+
+
+
+comb('',arr[0],0)
