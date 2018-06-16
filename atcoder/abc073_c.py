72614d96db8c5e45aa384785d16b6884ba0f2f8b
daleksprinter <b1015120@fun.ac.jp>
2018-06-17 02:32:53 +0900

    abc073_c.py

---
+++ atcoder/abc073_c.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+dic = {}
+for i in range(n):
+	tmp = int(input())
+	if dic.has_key(tmp):
+		dic[tmp] += 1
+	else :
+		dic[tmp] = 1
+
+count = 0 
+for i in dic.values():
+	count += i%2
+
+print count
