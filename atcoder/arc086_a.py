8d8df07d5883b9789badd0fb90e5e5bcba928586
daleksprinter <b1015120@fun.ac.jp>
2018-06-16 16:24:49 +0900

    arc086_a.py

---
+++ atcoder/arc086_a.py
@@ -0,0 +1,21 @@
+import Queue
+n,k = map(int,raw_input().split())
+arr = map(int,raw_input().split())
+
+counts = {}
+for i in arr:
+	if i not in counts:
+		counts[i] = 1
+	else :
+		counts[i] += 1
+
+queue = Queue.PriorityQueue()
+
+for i in counts.values():
+	queue.put(i)
+
+ans = 0
+while queue.qsize() > k :
+	ans += queue.get()
+
+print ans
