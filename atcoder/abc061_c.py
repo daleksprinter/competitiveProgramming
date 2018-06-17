500154f94fc4b51a12a12839e59a36403cbb4f43
daleksprinter <b1015120@fun.ac.jp>
2018-06-17 21:22:11 +0900

    abc061_c.py

---
+++ atcoder/abc061_c.py
@@ -0,0 +1,16 @@
+n,k = map(int,raw_input().split())
+
+arr = [0 for i in range(100001)]
+
+for i in range(n):
+	a,b = map(int,raw_input().split())
+	arr[a] += b
+
+i = 0
+while k > 0 :
+	i += 1
+	k -= arr[i]
+	
+
+print i
+
