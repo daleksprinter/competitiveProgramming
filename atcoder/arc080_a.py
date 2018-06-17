82707d24ad86a392452c417d25288a1e3a22813b
daleksprinter <b1015120@fun.ac.jp>
2018-06-17 20:52:53 +0900

    arc080_a.py

---
+++ atcoder/arc080_a.py
@@ -0,0 +1,25 @@
+n = int(input())
+
+arr = map(int,raw_input().split())
+
+dic = [0,0,0]
+
+for i in arr:
+	if i%4==0:
+		dic[2] += 1
+	elif i%2 == 0:
+		dic[1] += 1
+	else :
+		dic[0] += 1
+
+
+if dic[2]+1 <= dic[0] and 0 < dic[1]:
+	print 'No'
+elif dic[2]+1 == dic[0] and dic[1] == 0:
+	print 'Yes'
+
+elif dic[2] >= dic[0] :
+	print 'Yes'
+
+else :
+	print 'No'
