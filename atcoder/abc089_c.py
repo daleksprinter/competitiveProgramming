54bf81ffe8e16a2af79b2b2fb9b4066b329abc2b
daleksprinter <b1015120@fun.ac.jp>
2018-06-17 16:40:38 +0900

    abc089_c.py

---
+++ atcoder/abc089_c.py
@@ -0,0 +1,16 @@
+import itertools
+n = int(raw_input())
+
+d = {'M':0,'A':0,'R':0,'C':0,'H':0}
+
+for i in range(n):
+	s = raw_input()
+	if d.has_key(s[0]):
+		d[s[0]] += 1
+
+count = 0
+
+for comb in list(itertools.combinations(d.keys(),3)):
+	count += d[comb[0]]* d[comb[1]]*d[comb[2]]
+
+print count
