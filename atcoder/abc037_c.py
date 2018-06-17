7d832d0fadf0e193b8f845bcea6140b45ca0acc4
daleksprinter <b1015120@fun.ac.jp>
2018-06-17 21:43:55 +0900

    abc037_c.py

---
+++ atcoder/abc037_c.py
@@ -0,0 +1,19 @@
+n,k = map(int,raw_input().split())
+
+arr = map(int,raw_input().split())
+
+cumulative_sum = []
+
+s = 0
+cumulative_sum.append(0)
+
+for i in range(n):
+	cumulative_sum.append(s+arr[i])
+	s += arr[i]
+
+ans = 0
+for i in range(k,n+1):
+	ans += (cumulative_sum[i]-cumulative_sum[i-k])
+
+print ans
+
