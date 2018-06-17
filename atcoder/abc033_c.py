0ae90c96193a29857b03e44581605de68aaac4fc
daleksprinter <b1015120@fun.ac.jp>
2018-06-17 22:20:40 +0900

    abc033_c.py

---
+++ atcoder/abc033_c.py
@@ -0,0 +1,22 @@
+s = raw_input()
+l = len(s)
+
+stk = []
+ans = 0
+for i in range(l):
+	if s[i] == '*':
+		next
+	elif s[i] == '+':
+		if 0 in stk:
+			del stk[:]
+		else :
+			ans += 1
+			del stk[:]
+
+	else:
+		stk.append(int(s[i]))
+
+if not 0 in stk:
+	ans += 1
+
+print ans
