5ffbd485df4dbaab10bfda2c1cd1af1661de6c4d
daleksprinter <b1015120@fun.ac.jp>
2018-06-18 20:42:33 +0900

    abc032_c.py

---
+++ atcoder/abc032_c.py
@@ -0,0 +1,28 @@
+n,k = map(int,raw_input().split())
+
+arr = []
+
+for i in range(n):
+	arr.append(int(input()))
+
+if 0 in arr:
+	print n
+
+else :
+	ans = 0
+	right = 0
+	product = 1
+	for left in range(n) :
+		while right < n and product * arr[right] <= k :
+			product *= arr[right]
+			right += 1
+
+		ans = max(ans,right - left)
+
+		if right == left :
+			right += 1
+		else :
+			product /= arr[left]
+
+	print ans
+
