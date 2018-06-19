227b67c483baeecaa7fefc4ce5fe62dccfcc2858
daleksprinter <b1015120@fun.ac.jp>
2018-06-19 09:33:18 +0900

    abc040_c.py

---
+++ atcoder/abc040_c.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+arr = map(int,raw_input().split())
+
+dp = []
+
+dp.append(0)
+dp.append(abs(arr[0] - arr[1]))
+
+for i in range(2,n):
+	dp.append(min( dp[i-2] + abs(arr[i]-arr[i-2]) , dp[i-1] + abs(arr[i]-arr[i-1]) ))
+
+print dp[n-1]
