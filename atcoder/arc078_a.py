f9f79c0fb7874a15a4e754d849c01510e885a595
daleksprinter <b1015120@fun.ac.jp>
2018-06-21 14:19:16 +0900

    arc078_a.py

---
+++ atcoder/arc078_a.py
@@ -0,0 +1,14 @@
+import numpy
+
+n = int(input())
+
+arr = map(int,raw_input().split())
+
+sumarr = numpy.cumsum(arr)
+l = len(sumarr)
+ans = float("inf")
+for i in range(n-1):
+	ans = min(ans,abs(sumarr[l-1]-2*sumarr[i]))
+
+print ans
+
