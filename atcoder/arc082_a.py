873ffe32813cd2005dad219b77f981ad16f44c24
daleksprinter <b1015120@fun.ac.jp>
2018-06-21 13:13:13 +0900

    arc082_a.py

---
+++ atcoder/arc082_a.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+arr = map(int,raw_input().split())
+
+dic = [0 for i in range(10**5)]
+
+for i in arr:
+	dic[i] += 1
+
+ans = 0
+for i in range(10**5-2):
+	ans = max(ans,dic[i]+dic[i+1]+dic[i+2])
+
+print ans
