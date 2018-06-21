c3b48454a18152f66e8b30d7a9a15f41a6e4ac74
daleksprinter <b1015120@fun.ac.jp>
2018-06-21 14:42:15 +0900

    arc075_a.py

---
+++ atcoder/arc075_a.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+arr = []
+
+for i in range(n):
+	arr.append(int(input()))
+
+arr.sort()
+
+ans = sum(arr)
+
+if ans%10 == 0:
+	ind = 0
+	while  ind < n and arr[ind] % 10 == 0:
+		ind += 1
+
+	if ind == n:
+		print 0
+
+	else :
+		print ans - arr[ind]
+else:
+	print ans
