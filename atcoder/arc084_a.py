51b920a8b751696f5d561634d96bda163553b9ca
daleksprinter <b1015120@fun.ac.jp>
2018-06-22 10:57:51 +0900

    arc084_a.py

---
+++ atcoder/arc084_a.py
@@ -0,0 +1,15 @@
+import bisect
+
+n = int(input())
+a = map(int,raw_input().split())
+a.sort()
+b = map(int,raw_input().split())
+b.sort()
+c = map(int,raw_input().split())
+c.sort()
+
+count = 0
+for i in b:
+	count += bisect.bisect_left(a,i)*(n-bisect.bisect_right(c,i))
+
+print count 
