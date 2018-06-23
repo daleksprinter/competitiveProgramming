a2469b570ff1a03ede0f9b1e06d2b50f0cd2a96f
daleksprinter <b1015120@fun.ac.jp>
2018-06-24 01:07:07 +0900

    arc087_a.py

---
+++ atcoder/arc087_a.py
@@ -0,0 +1,21 @@
+import collections
+n = int(input())
+
+arr = map(int,raw_input().split())
+
+counter = collections.Counter(arr)
+
+ans = 0 
+
+for item in counter.items() :
+	if item[0] == item[1]:
+		next
+
+	elif item[1] < item[0]:
+		ans += item[1]
+
+	else :
+		ans += item[1]-item[0]
+
+print ans
+
