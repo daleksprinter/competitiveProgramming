50ab5d87fc3a2b8b87a5fad6b4b080d25617d4d6
daleksprinter <b1015120@fun.ac.jp>
2018-06-23 23:24:37 +0900

    arc059_a.py

---
+++ atcoder/arc059_a.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+arr = map(int,raw_input().split())
+
+ma = max(arr)
+mi = min(arr)
+
+ans = float("inf")
+
+for i in range(mi,ma+1):
+	count = 0
+	for j in range(n):
+
+		count += (i - arr[j]) * (i - arr[j])
+
+	ans = min(ans,count)
+
+print ans
