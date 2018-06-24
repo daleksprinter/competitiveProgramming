50742eba69bb6b52613927e36adc321907618f20
daleksprinter <b1015120@fun.ac.jp>
2018-06-24 23:22:40 +0900

    abc099_c.py

---
+++ atcoder/abc099_c.py
@@ -0,0 +1,34 @@
+n = int(input())
+
+arr6 = []
+for i in range(10):
+	arr6.append(6**i)
+
+arr9 = []
+for i in range(10):
+	arr9.append(9**i)
+
+
+ans = float("inf")
+for i in range(n+1):
+	count = 0
+	tmp = i
+	while tmp > 0:
+		t = 0
+		while arr6[t] <= tmp:
+			t+=1
+
+		tmp -= arr6[t-1]
+		count += 1
+
+	tmp = n-i
+	while tmp > 0:
+		t = 0
+		while arr9[t] <= tmp:
+			t += 1
+		tmp -= arr9[t-1]
+		count += 1
+
+	ans = min(ans,count)
+
+print ans
