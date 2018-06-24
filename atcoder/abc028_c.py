02fcccdd29b3e8864224387b5be3d391a41c138a
daleksprinter <b1015120@fun.ac.jp>
2018-06-24 23:31:14 +0900

    abc028_c.py

---
+++ atcoder/abc028_c.py
@@ -0,0 +1,10 @@
+import itertools
+arr = map(int,raw_input().split())
+
+results = []
+for item in itertools.combinations(range(5),3):
+	results.append(arr[item[1]]+arr[item[0]]+arr[item[2]])
+
+results.sort()
+
+print results[-3]
