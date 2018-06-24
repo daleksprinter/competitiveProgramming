e67807e3a76078052d056ac2fcaae9bd543550d9
daleksprinter <b1015120@fun.ac.jp>
2018-06-24 21:52:17 +0900

    tdpc_contest.py

---
+++ atcoder/tdpc_contest.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+arr = map(int,raw_input().split())
+
+dp = [[-1 for i in range(10001)] for j in range(n+1)]
+
+dp[0][0] = 0
+
+for i in range(n):
+	for j in range(10001):
+		if dp[i][j] != -1 and j+arr[i] < 10001:
+			dp[i+1][j] = 0
+			dp[i+1][j + arr[i]] = 0
+
+
+print dp[n].count(0)
