bea212e4efa6f54d0ff040ed859fce232701beb8
daleksprinter <b1015120@fun.ac.jp>
2018-06-24 15:39:08 +0900

    agc005_a.py

---
+++ atcoder/agc005_a.py
@@ -0,0 +1,19 @@
+s = raw_input()
+
+stk = []
+
+ans = 0
+
+for string in s:
+	if string == 'S':
+		stk.append('S')
+
+	else :
+		if len(stk) != 0:
+			stk.pop()
+
+		else :
+			ans += 1
+
+print ans + len(stk)
+
