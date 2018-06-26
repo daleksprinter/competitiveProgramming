9121edabfeba290e48ba69d441bc9480e519e6c6
daleksprinter <b1015120@fun.ac.jp>
2018-06-26 12:35:02 +0900

    abc085_d.py

---
+++ atcoder/abc085_d.py
@@ -0,0 +1,25 @@
+import math
+n,h = map(int,raw_input().split())
+
+arra = []
+arrb = []
+
+
+for i in range(n):
+	a , b = map(int,raw_input().split())
+	arra.append(a)
+	arrb.append(b)
+
+arrb.sort()
+
+l = len(arrb) - 1 
+m = max(arra)
+count = 0
+while arrb[l] > m and -1 < l and h > 0:
+	h = max(0,h-arrb[l])
+	l -= 1
+	count += 1
+
+print count + -(h*-1 // m)
+
+
