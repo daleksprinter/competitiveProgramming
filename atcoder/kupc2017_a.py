8c06c9b27626c7221beece8288ef138c930a16f4
daleksprinter <b1015120@fun.ac.jp>
2018-06-29 01:59:01 +0900

    kupc2017_a.py

---
+++ atcoder/kupc2017_a.py
@@ -0,0 +1,16 @@
+n,k = map(int,raw_input().split())
+
+
+arr = map(int,raw_input().split())
+
+arr.sort()
+arr.reverse()
+
+ans = 0
+i= 0
+while k > 0 and i < n:
+	k -= arr[i]
+	ans += 1
+	i+=1
+
+print ans if k <= 0 else -1
