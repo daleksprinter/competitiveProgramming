31dd7d230ebc2a5909c0c388987b40ffd2054af6
daleksprinter <b1015120@fun.ac.jp>
2018-07-01 23:41:25 +0900

    agc014_a.py

---
+++ atcoder/agc014_a.py
@@ -0,0 +1,20 @@
+a,b,c = map(int,raw_input().split())
+
+if a == b and b == c :
+	print 0 if a %2 == 1 else -1
+
+else : 
+	count = 0
+	while True:
+		if a % 2 == 1 or b%2 == 1 or c%2 ==1:
+			print count 
+			break
+		else :
+			a_tmp = b/2 + c/2
+			b_tmp = a/2 + c/2
+			c_tmp = a/2 + b/2
+			a = a_tmp
+			b = b_tmp
+			c = c_tmp
+			count += 1
+
