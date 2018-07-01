41ad25e1ddd6bb6a4e948e1cab7bfff7ac6900af
daleksprinter <b1015120@fun.ac.jp>
2018-07-01 23:02:00 +0900

    agc017_a.py

---
+++ atcoder/agc017_a.py
@@ -0,0 +1,28 @@
+import math
+n, p = map(int,raw_input().split())
+
+arr = map(int,raw_input().split())
+
+odd = 0
+even = 0
+
+for i in range(n):
+	if arr[i] %2 == 0 :
+		even += 1
+	else :
+		odd += 1
+
+def P(n, r):
+	return math.factorial(n)//math.factorial(n-r)
+
+def C(n, r):
+	return P(n, r)//math.factorial(r)
+
+oddnum = 0
+for i in range(1,odd+1,2):
+	oddnum += C(odd,i)
+
+if p == 1:
+	print oddnum * 2**even
+else :
+	print 2**n-(oddnum*2**even)
