4b8a7cb4efb14377b503c627f6e7c7245e5c8894
daleksprinter <b1015120@fun.ac.jp>
2018-07-11 17:35:52 +0900

    arc095_a.py

---
+++ atcoder/arc095_a.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+arr = map(int,raw_input().split())
+
+arrb = arr[:]
+arrb.sort()
+a = n/2-1
+b = n/2
+
+for i in range(n):
+	if arr[i] >= arrb[b]:
+		print arrb[a]
+	else :
+		print arrb[b]
