a0b925c63163b7ed0cc9df1b1061afaf61e824cb
daleksprinter <b1015120@fun.ac.jp>
2018-07-12 00:31:22 +0900

    abc039_c.py

---
+++ atcoder/abc039_c.py
@@ -0,0 +1,10 @@
+import re
+
+s = raw_input()
+keys = ['Do','Do','Re','Re','Mi','Fa','Fa','So','So','La','La','Si']
+keyboard = "WBWBWWBWBWBWWBWBWWBWBWBWWBWBWWBWBWBWWBWBWWBWBWBW"
+
+regex = (r'%s'%s)
+pattern = re.compile(regex)
+
+print keys[pattern.search(keyboard).start()%12]
