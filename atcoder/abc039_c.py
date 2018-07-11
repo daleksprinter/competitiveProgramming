516f1e1aa26a63068746d30d8ceaaecdb96ee477
daleksprinter <b1015120@fun.ac.jp>
2018-07-12 00:28:58 +0900

    abc039_c.py

--- atcoder/abc039_c.py
+++ atcoder/abc039_c.py
@@ -2,9 +2,15 @@ import re
 
 s = raw_input()
 keys = ['Do','Do','Re','Re','Mi','Fa','Fa','So','So','La','La','Si']
-keyboard = "WBWBWWBWBWBWWBWBWWBWBWBWWBWBWWBWBWBWWBWBWWBWBWBW"
+
+key = "WBWBWWBWBWBW"
+
+key *= 5
 
 regex = (r'%s'%s)
+
 pattern = re.compile(regex)
 
-print keys[pattern.search(keyboard).start()%12]
+print keys[pattern.search(key).start()%12]
+
+
