aaa2959c317749ecab585083df86e073a716bfdd
daleksprinter <b1015120@fun.ac.jp>
2018-07-11 23:16:38 +0900

    abc041_c.py

---
+++ atcoder/abc041_c.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+arr = map(int,raw_input().split())
+
+arrb = []
+
+for i in range(n):
+	arrb.append([arr[i],i+1])
+
+arrb.sort()
+
+for i in range(n-1,-1,-1):
+	print arrb[i][1]
