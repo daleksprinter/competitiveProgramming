4f3f3228c380597251ece7dd19ef1814973e9deb
daleksprinter <b1015120@fun.ac.jp>
2018-07-11 23:53:56 +0900

    arc073_a.py

---
+++ atcoder/arc073_a.py
@@ -0,0 +1,15 @@
+n, t = map(int,raw_input().split())
+
+arr = map(int,raw_input().split())
+
+ans = 0
+for i in range(n-1):
+	if arr[i] + t > arr[i+1]:
+		ans += arr[i+1]-arr[i]
+
+	else :
+		ans += t
+
+print ans+t
+
+
