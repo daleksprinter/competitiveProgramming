11279d5c97d0808ee5059fc88c3b3eb3b056ff6c
daleksprinter <b1015120@fun.ac.jp>
2018-07-11 22:52:24 +0900

    abc038_c.py

---
+++ atcoder/abc038_c.py
@@ -0,0 +1,18 @@
+import math
+n = int(input())
+
+arr = map(int,raw_input().split())
+
+l = 0 
+r = 0 
+
+ans = 0
+while r < n:
+	while r+1 < n and arr[r+1] > arr[r]:
+		r += 1
+
+	ans += sum(range(r-l+2))
+	r += 1
+	l = r
+
+print ans
