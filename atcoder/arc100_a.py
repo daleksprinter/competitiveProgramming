d1e572b151b92e3516b8a3596b3c1674c91a8530
daleksprinter <b1015120@fun.ac.jp>
2018-07-11 17:16:01 +0900

    arc100_a.py

---
+++ atcoder/arc100_a.py
@@ -0,0 +1,17 @@
+import numpy as np
+n = int(input())
+
+arr = map(int,raw_input().split())
+
+arrb = []
+
+for i in range(n):
+	arrb.append(arr[i]-i)
+
+med = np.median(arrb)
+
+ans = 0
+for i in range(n):
+	ans += abs(arrb[i]-med)
+
+print int(ans)
