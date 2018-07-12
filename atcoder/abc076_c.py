c04d98566419e769b28925757249daff9280a422
daleksprinter <b1015120@fun.ac.jp>
2018-07-13 02:01:31 +0900

    abc076_c.py

---
+++ atcoder/abc076_c.py
@@ -0,0 +1,24 @@
+import sys
+s = raw_input()
+t = raw_input()
+
+ls = len(s)
+lt = len(t)
+if ls < lt :
+	print 'UNRESTORABLE'
+	sys.exit()
+
+for i in range(ls-lt,-1,-1):
+	count = 0
+	for j in range(lt):
+		if s[i+j] == '?' or s[i+j] == t[j]:
+			count += 1
+		else :
+			break
+	if count == lt:
+
+		print s[:i].replace('?','a')+t+s[i+lt:].replace('?','a')
+		sys.exit()
+
+print 'UNRESTORABLE'
+
