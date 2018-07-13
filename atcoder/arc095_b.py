8fd3876af7977738b1b12a307f7358bb8aadd28d
daleksprinter <b1015120@fun.ac.jp>
2018-07-13 18:30:26 +0900

    arc095_b.py

---
+++ atcoder/arc095_b.py
@@ -0,0 +1,13 @@
+import numpy as np
+n = int(input())
+
+arr = map(int,raw_input().split())
+
+arr.sort()
+mx = arr.pop()
+
+def getNearestValue(list, num):
+	idx = np.abs(np.asarray(list) - num).argmin()
+	return list[idx]
+
+print mx,getNearestValue(arr,mx/2.0)
