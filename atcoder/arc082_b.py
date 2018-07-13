c025c3b6297abf3527bf7b7373fc18918eff422d
daleksprinter <b1015120@fun.ac.jp>
2018-07-13 18:03:08 +0900

    arc082_b.py

---
+++ atcoder/arc082_b.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+arr = map(int,raw_input().split())
+
+ans = 0
+
+for i in range(n-1):
+	if arr[i] == i+1:
+		arr[i],arr[i+1] = arr[i+1],arr[i]
+		ans += 1
+
+print ans if arr[n-1] != n else ans + 1
