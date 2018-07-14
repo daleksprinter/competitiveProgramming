f15ad58d3032c58d112d42e88b7bc28fdc603790
daleksprinter <b1015120@fun.ac.jp>
2018-07-14 13:34:48 +0900

    tkppc3_c.py

---
+++ atcoder/tkppc3_c.py
@@ -0,0 +1,28 @@
+import sys
+n,p = map(int,raw_input().split())
+arr = map(int,raw_input().split())
+
+l = 0 
+r = 0 
+
+ans = arr[0]
+
+while r < n:
+	if ans == p:
+		print 'Yay!'
+		sys.exit()
+
+	elif r+1 < n and ans * arr[r+1] <= p:
+		r += 1
+		ans *= arr[r]
+
+	else  :
+		ans /= arr[l]
+		l += 1
+		if l == n:
+			break
+
+
+
+print ':('
+
