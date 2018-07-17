4ac620d7bfec73cf9ef2e2c0f856d8b26d06e658
daleksprinter <b1015120@fun.ac.jp>
2018-07-18 01:25:41 +0900

    codefestival_2016_qualA_c.py

---
+++ atcoder/codefestival_2016_qualA_c.py
@@ -0,0 +1,16 @@
+
+aord = ord("a")
+s = list(raw_input())
+k = int(input())
+i = 0
+
+while k > 0 and i < len(s):
+	if k >= ord('z') - ord(s[i]) + 1:
+		if s[i] != "a":
+			k -= ord('z') - ord(s[i]) + 1
+			s[i] = "a"
+
+	i += 1
+k = k%26
+s[len(s)-1] = chr(ord(s[len(s)-1]) + k)
+print ''.join(s)
