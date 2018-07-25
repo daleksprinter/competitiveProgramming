27c7ec4bfcd7f5a1f5ef01b4d11749d09e723c82
daleksprinter <b1015120@fun.ac.jp>
2018-07-25 16:10:29 +0900

    arc077_a.py

---
+++ atcoder/arc077_a.py
@@ -0,0 +1,22 @@
+import collections
+n = int(input())
+
+arr = map(int,raw_input().split())
+
+deque = collections.deque()
+
+for i in range(n):
+	if i % 2 == 0:
+		deque.appendleft(arr[i])
+
+	else :
+		deque.append(arr[i])
+
+for i in range(n) :
+	arr[i] = str(deque.pop())
+
+if i % 2 == 1:
+	print ' '.join(arr)
+
+else :
+	print ' '.join(reversed(arr))
