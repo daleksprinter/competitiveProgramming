0ca3ebe098e047cdbb3516a7dc6c91e1453d804c
daleksprinter <b1015120@fun.ac.jp>
2018-07-25 15:07:04 +0900

    agc025_a.py

---
+++ atcoder/agc025_a.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+def digitsum(x):
+	tmp = x
+	ans = 0
+	while tmp > 0:
+		ans += tmp % 10
+		tmp /= 10
+
+	return ans
+
+ans = float("inf")
+for i in range(1,(n/2)+1):
+	ans = min(ans,digitsum(i) + digitsum(n-i))
+
+print ans
