d3ff1d01e127747b7acc9ba5ccb3af31d722a1a0
daleksprinter <b1015120@fun.ac.jp>
2018-07-26 20:26:21 +0900

    arc068_b.py

---
+++ atcoder/arc068_b.py
@@ -0,0 +1,28 @@
+import collections
+n = int(input())
+
+arr = map(int,raw_input().split())
+l = len(arr)
+counter = collections.Counter(arr)
+
+c = 0
+
+for item in counter.items() :
+	if item[1] > 2:
+		if item[1] % 2 == 1 :
+			l -= item[1] - 1
+
+		else:
+			l -= item[1] - 2
+
+	if item[1] % 2 == 0 :
+		c += 1
+
+if c % 2 == 0:
+	l -= c
+
+else :
+	l -= c+1
+
+print l
+
