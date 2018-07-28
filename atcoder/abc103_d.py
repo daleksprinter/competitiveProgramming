11c3a5cf583d98a62eba2250eb453b968f66b468
daleksprinter <b1015120@fun.ac.jp>
2018-07-28 17:26:36 +0900

    abc103_d.py

---
+++ atcoder/abc103_d.py
@@ -0,0 +1,22 @@
+n,m = map(int,raw_input().split())
+
+arr = []
+
+for i in range(m):
+	arr.append(map(int,raw_input().split()))
+
+arr = sorted(arr, key=lambda x: x[1])
+
+
+ans = 0
+t = 0
+
+
+for i in range(m):
+	if arr[i][0] >= t:
+		ans += 1
+		t = arr[i][1]
+
+print ans
+
+
