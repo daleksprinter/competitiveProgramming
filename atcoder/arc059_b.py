fbbc770cb5b9761bc1b58803de414bdf114adbc7
daleksprinter <b1015120@fun.ac.jp>
2018-07-30 00:07:40 +0900

    arc059_b.py

---
+++ atcoder/arc059_b.py
@@ -0,0 +1,24 @@
+import sys
+s = list(raw_input())
+
+l = len(s)
+
+if l == 2:
+	if s[0] == s[1]:
+		print "1 2"
+		sys.exit()
+
+	else:
+		print '-1 -1'
+		sys.exit()
+
+for i in range(l-2):
+	t = []
+	for j in range(3):
+		if s[i + j] in t:
+			print i+1,i+3
+			sys.exit()
+		else :
+			t.append(s[i + j])
+
+print "-1 -1"
