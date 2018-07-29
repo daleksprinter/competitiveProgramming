51cf3dc7aed1057bd1149da590f778a139c0d37f
daleksprinter <b1015120@fun.ac.jp>
2018-07-30 07:33:15 +0900

    abc100_d.py

---
+++ atcoder/abc100_d.py
@@ -0,0 +1,22 @@
+n,m = map(int,raw_input().split())
+
+arr = []
+
+for i in range(n):
+	arr.append(map(int,raw_input().split()))
+
+v = [[1,1,1],[1,1,-1],[1,-1,-1],[1,-1,1],[-1,-1,-1],[-1,1,1],[-1,1,-1],[-1,-1,1]]
+
+ans = 0
+
+for i in range(8):
+	tmp = []
+	for j in range(n):
+		tmp.append(arr[j][0] * v[i][0] + arr[j][1] * v[i][1] + arr[j][2] * v[i][2])
+
+	tmp.sort()
+	ans = max(ans,sum(tmp[n-m:]))
+
+print ans
+
+
