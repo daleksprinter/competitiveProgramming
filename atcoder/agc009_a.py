3e856ee786fb45ca0b1a09f91e6f273d126ae798
daleksprinter <b1015120@fun.ac.jp>
2018-08-02 03:39:59 +0900

    agc009_a.py

---
+++ atcoder/agc009_a.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+a = []
+b = []
+
+for i in range(n):
+	at, bt = map(int,raw_input().split())
+
+	a.append(at)
+	b.append(bt)
+
+ans = 0
+for i in range(n-1,-1,-1):
+	if (a[i] + ans) % b[i] == 0:
+		next
+
+	else :
+		ans += (((a[i] + ans) / b[i]) + 1) * b[i] - (a[i] + ans)
+
+print ans
