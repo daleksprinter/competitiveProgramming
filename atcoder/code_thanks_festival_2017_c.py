871b2da6919a9e0cee3abae65100cca6f001e3e9
daleksprinter <b1015120@fun.ac.jp>
2018-08-02 16:48:32 +0900

    code_thanks_festival_2017_c.py

---
+++ atcoder/code_thanks_festival_2017_c.py
@@ -0,0 +1,17 @@
+import Queue
+n,k = map(int,raw_input().split())
+
+q = Queue.PriorityQueue()
+
+ans = 0
+for i in range(n):
+	q.put(map(int,raw_input().split()))
+
+while k > 0:
+	tmp = q.get()
+	ans += tmp[0]
+	tmp[0] += tmp[1]
+	q.put(tmp)
+	k -= 1
+
+print ans
