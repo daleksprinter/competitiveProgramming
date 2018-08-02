550e19d61c6b3966b136069a18af372ccafb449a
daleksprinter <b1015120@fun.ac.jp>
2018-08-02 17:12:09 +0900

    arc071_a.py

---
+++ atcoder/arc071_a.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+arr = [[0 for i in range(n)] for j in range(26)]
+
+for i in range(n):
+	s = list(raw_input())
+	for t in s:
+		arr[ord(t) - ord("a")][i] += 1
+ans = ''
+for i in range(26):
+	ans += chr(ord("a") + i) * min(arr[i])
+
+print ans
+
