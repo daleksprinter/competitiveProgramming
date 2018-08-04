b30aebf59a36aa4ee874f12dd484876dd5c392e3
daleksprinter <b1015120@fun.ac.jp>
2018-08-04 19:23:35 +0900

    agc011_a.py

---
+++ atcoder/agc011_a.py
@@ -0,0 +1,26 @@
+n,c,k = map(int,raw_input().split())
+
+
+t = []
+for i in range(n):
+	t.append(int(input()))
+
+t.sort()
+
+ans = 0
+depart = t[0]
+people = 0
+
+i = 0
+while i < n:
+	if t[i] <= depart + k and people < c :
+		people += 1
+		i += 1
+
+	else :
+		ans += 1
+		people = 0
+		depart = t[i]
+
+print ans + 1
+
