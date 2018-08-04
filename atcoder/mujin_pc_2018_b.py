9b4ac7c303b802e4f97737cf0c920bb637776b1d
daleksprinter <b1015120@fun.ac.jp>
2018-08-04 21:04:12 +0900

    mujin_pc_2018_b.py

---
+++ atcoder/mujin_pc_2018_b.py
@@ -0,0 +1,19 @@
+a = int(input())
+
+s = raw_input()
+
+if a == 0:
+	print 'Yes'
+	quit()
+
+for i in range(len(s)):
+	if s[i] == '-':
+		a -= 1
+	else :
+		a += 1
+
+	if a == 0:
+		print 'Yes'
+		quit()
+
+print 'No' 
