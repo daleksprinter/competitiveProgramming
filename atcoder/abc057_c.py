853078afdce19aea51796f5bc46164c234418551
daleksprinter <b1015120@fun.ac.jp>
2018-08-04 19:46:37 +0900

    abc057_c.py

---
+++ atcoder/abc057_c.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+a = 1
+
+def digitlen(i):
+	t = i
+	ret = 0	
+	while t > 0 :
+		t /= 10
+		ret += 1
+	return ret
+
+
+ans = float("inf")
+
+while a**2 <= n:
+	if n%a == 0:
+		b = n/a
+		ans = min(ans, max(digitlen(a),digitlen(b)))
+
+	a += 1
+
+print ans
+
