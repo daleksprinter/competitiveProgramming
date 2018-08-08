1cceefb371f7ad6406638eab3151cf21e3026088
daleksprinter <b1015120@fun.ac.jp>
2018-08-09 03:41:11 +0900

    arc092_a.py

---
+++ atcoder/arc092_a.py
@@ -0,0 +1,33 @@
+n = int(input())
+
+red = []
+
+for i in range(n):
+	red.append(map(int,raw_input().split()))
+
+blue = []
+
+for i in range(n):
+	blue.append(map(int,raw_input().split()))
+
+
+redflug = []
+for i in range(n):
+	redflug.append(0)
+
+blue.sort()
+
+ans = 0
+for i in range(n):
+	t = -1
+	maxy = -1
+	for j in range(n):
+		if redflug[j] == 0 and blue[i][0] > red[j][0] and blue[i][1] > red[j][1] and red[j][1] > maxy :
+			t = j
+			maxy = red[j][1]
+
+	if t != -1 :
+		redflug[t] = 1
+		ans += 1
+
+print ans
