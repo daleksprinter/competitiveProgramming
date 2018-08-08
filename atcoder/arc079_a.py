2700169d154ad5487fdc990205223eb56c8b3298
daleksprinter <b1015120@fun.ac.jp>
2018-08-09 07:50:08 +0900

    arc079_a.py

---
+++ atcoder/arc079_a.py
@@ -0,0 +1,19 @@
+n,m = map(int,raw_input().split())
+
+stomid = [0]*n
+midtog = [0]*n
+
+for i in range(m):
+	f,t = map(int,raw_input().split())
+	if f == 1:
+		stomid[t-1] = 1
+
+	elif t == n:
+		midtog[f-1] = 1 
+
+for i in range(n):
+	if stomid[i] == midtog[i] == 1 :
+		print 'POSSIBLE'
+		quit()
+
+print 'IMPOSSIBLE'
