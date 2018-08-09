40431c7657c21e5c7d950277f70610f18fd0639b
daleksprinter <b1015120@fun.ac.jp>
2018-08-10 01:26:25 +0900

    arc090_a.py

--- atcoder/arc090_a.py
+++ atcoder/arc090_a.py
@@ -1,16 +1,18 @@
 n = int(input())
 
-arr = [map(int,raw_input().split()) for i in range(2)]
+arr = []
+arr.append(map(int,raw_input().split()))
+arr.append(map(int,raw_input().split()))
 
-ans = [[0 for i in range(n)] for j in range(2)]
+dp = [[0 for i in range(n)] for j in range(2)]
 
-ans[0][0] = arr[0][0]
-ans[1][0] = arr[0][0] + arr[1][0]
+dp[0][0] = arr[0][0]
+for i in range(1,n):
+	dp[0][i] = dp[0][i-1] + arr[0][i]
 
-for i in range(n-1):
-	ans[0][i+1] = ans[0][i] + arr[0][i+1]
+dp[1][0] = dp[0][0] + arr[1][0]
 
-for i in range(n-1):
-	ans[1][i+1] = max(ans[0][i+1] + arr[1][i+1], ans[1][i] + arr[1][i+1])
+for i in range(1,n):
+	dp[1][i] = max(dp[1][i-1] + arr[1][i],dp[0][i] + arr[1][i])
 
-print ans[1][n-1]
+print dp[1][n-1]
