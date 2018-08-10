6163c424ea9d9726688c637b88776c8a57c27335
daleksprinter <b1015120@fun.ac.jp>
2018-08-11 05:04:23 +0900

    arc093_a.py

---
+++ atcoder/arc093_a.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+
+arr = []
+arr.append(0) 
+arr.extend(map(int,raw_input().split()))
+arr.append(0)
+
+distance = 0
+for i in range(n+1):
+	distance += abs(arr[i + 1] - arr[i])
+
+for i in range(0,n):
+	print distance - (abs(arr[i + 1] - arr[i]) + abs(arr[i + 2] - arr[i + 1])) + abs(arr[i + 2] - arr[i])
