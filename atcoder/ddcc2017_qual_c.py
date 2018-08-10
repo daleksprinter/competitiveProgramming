392a6597d3cd997e740a6c5f149f5ce356cb3ab2
daleksprinter <b1015120@fun.ac.jp>
2018-08-11 06:18:39 +0900

    ddcc2017_qual_c.py

---
+++ atcoder/ddcc2017_qual_c.py
@@ -0,0 +1,24 @@
+n,c = map(int,raw_input().split())
+
+arr = []
+
+for i in range(n):
+	arr.append(int(input()))
+
+arr.sort()
+
+l = 0
+r = n-1
+ans = 0
+
+while l < r:
+	if arr[r] + arr[l] + 1 <= c:
+		ans += 1
+		l += 1
+		r -= 1
+
+	else :
+		ans += 1
+		r -= 1
+
+print ans + 1 if l == r else ans 
