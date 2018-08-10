9cc41524a05846ca6b22e9bcd0d4ab02c0c62a86
daleksprinter <b1015120@fun.ac.jp>
2018-08-11 08:46:17 +0900

    abc080_d.py

---
+++ atcoder/abc080_d.py
@@ -0,0 +1,17 @@
+n, c = map(int,raw_input().split())
+
+arr = [[0 for i in range(c)] for j in range(10**5+1)]
+
+for i in range(n):
+	s,t,ch = map(int,raw_input().split())
+	s -= 1
+	ch -= 1
+	for i in range(s,t):
+		arr[i][ch] = 1
+
+ans = 0
+
+for i in range(10**5+1):
+	ans = max(ans,sum(arr[i]))
+
+print ans
