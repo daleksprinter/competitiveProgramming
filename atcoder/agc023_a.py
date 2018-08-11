fa93c083d295bbce22184f7bf10433c8b2cf9a34
daleksprinter <b1015120@fun.ac.jp>
2018-08-12 00:30:50 +0900

    agc023_a.py

---
+++ atcoder/agc023_a.py
@@ -0,0 +1,14 @@
+import numpy,collections
+n = int(input())
+
+arr = map(int,raw_input().split())
+
+cs = [0]
+cs.extend(numpy.cumsum(arr))
+count = collections.Counter(cs)
+
+ans = 0
+for i in count.values():
+	ans += i*(i-1)/2
+
+print ans
