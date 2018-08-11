c55e18c9b2e637a6b182f3273073b616176f0ee1
daleksprinter <b1015120@fun.ac.jp>
2018-08-11 23:02:45 +0900

    abc105_d.py

---
+++ atcoder/abc105_d.py
@@ -0,0 +1,21 @@
+import collections
+
+n,m = map(int,raw_input().split())
+
+arr = map(int,raw_input().split())
+
+sums = []
+sums.append(arr[0]%m)
+
+for i in range(1,n):
+	sums.append((sums[i-1] + arr[i])%m)
+
+
+
+c = collections.Counter(sums)
+ans = 0
+ans += sums.count(0)
+for i in c.values():
+	ans += i*(i-1)/2
+
+print ans
