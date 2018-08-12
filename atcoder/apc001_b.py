7cfcb89bcc6cca0ea13408cf070d9458b347b93d
daleksprinter <b1015120@fun.ac.jp>
2018-08-12 16:50:48 +0900

    apc001_b.py

---
+++ atcoder/apc001_b.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+a = map(int,raw_input().split())
+b = map(int,raw_input().split())
+
+a_t = 0
+b_t = 0
+
+for i in range(n):
+	if a[i] > b[i] :
+		a_t += (a[i] - b[i])
+
+	else:
+		b_t += (b[i]-a[i])/2
+
+print 'Yes' if a_t <= b_t else 'No'
