b58f8545d40f329212be25030d39722eecbe4c60
daleksprinter <b1015120@fun.ac.jp>
2018-08-12 19:10:48 +0900

    arc081_a.py

---
+++ atcoder/arc081_a.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+arr = map(int,raw_input().split())
+
+arr.sort()
+
+i = n-1
+
+lines = []
+
+while i > 0 and len(lines) < 2:
+	if arr[i] == arr[i-1]:
+		lines.append(arr[i])
+		i -= 2
+		next
+
+	else :
+		i -= 1
+
+if len(lines) == 2:
+	print lines[0] * lines[1]
+
+else :
+	print 0
