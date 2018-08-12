3a4489588b884b2d6e6aacfac451a5dc76ce6b67
daleksprinter <b1015120@fun.ac.jp>
2018-08-13 02:14:27 +0900

    arc076_a.py

---
+++ atcoder/arc076_a.py
@@ -0,0 +1,18 @@
+
+mod = 10**9+7
+n,m = map(int,raw_input().split())
+def factorial(x):
+	retval = 1
+	for i in range(2,x+1):
+		retval = (retval * i)% mod
+	return retval
+
+if abs(n-m) > 1:
+	print 0
+	quit()
+
+if max(n,m) > min(n,m):
+	print (factorial(max(n,m)) * factorial(min(n,m))) % mod
+
+else :
+	print ((factorial(n)**2)%mod * 2)%mod
