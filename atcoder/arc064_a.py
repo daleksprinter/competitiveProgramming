f508e00ff3d07f4272c0b781d645977867ead1ad
daleksprinter <b1015120@fun.ac.jp>
2018-08-14 08:38:38 +0900

    arc064_a.py

---
+++ atcoder/arc064_a.py
@@ -0,0 +1,17 @@
+n,m = map(int,raw_input().split())
+
+arr = map(int,raw_input().split())
+
+ans = 0
+for i in range(1,n):
+	if arr[i] + arr[i-1] > m:
+		t = min(arr[i],arr[i] + arr[i-1]-m)
+		arr[i] -= t
+		ans += t
+
+	if arr[i] + arr[i-1] > m:
+		t = min(arr[i-1],arr[i-1]-m)
+		arr[i-1] -= t
+		ans += t
+
+print ans
