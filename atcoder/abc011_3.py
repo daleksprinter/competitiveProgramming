db5e8571ef18afac8ee989c69f39f17f2f137006
daleksprinter <b1015120@fun.ac.jp>
2018-08-14 03:08:38 +0900

    abc011_3.py

---
+++ atcoder/abc011_3.py
@@ -0,0 +1,32 @@
+n = int(input())
+
+rejected = []
+
+for i in range(3):
+	rejected.append(int(input()))
+
+
+dp = [float("inf") for i in range(n+1)]
+
+for i in range(3):
+	if rejected[i] < n+1:
+		dp[rejected[i]] = -1
+
+
+if dp[n] == -1:
+	print 'NO'
+	quit() 
+
+dp[n] = 0
+
+i = n
+
+while i > 0 :
+	if dp[i] != -1:
+		for j in range(1,4):
+			if  i-j > -1 :
+				dp[i-j] = min(dp[i-j], dp[i] + 1)
+
+	i -= 1
+
+print 'YES' if dp[0] <= 100 else 'NO'
