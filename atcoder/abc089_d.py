1419a341565690dda7659ca215fe2b0ec7e11ac5
daleksprinter <b1015120@fun.ac.jp>
2018-08-14 13:26:47 +0900

    abc089_d.py

---
+++ atcoder/abc089_d.py
@@ -0,0 +1,32 @@
+h,w,d = map(int,raw_input().split())
+
+field = []
+
+for i in range(h):
+	field.append(map(int,raw_input().split()))
+
+distance = [0 for i in range(90001)]
+
+
+n = int(input())
+
+arr = [[] for i in range(90001)]
+
+inputs = []
+
+for i in range(n):
+	inputs.append(map(int,raw_input().split()))
+
+
+for i in range(h):
+	for j in range(w):
+		arr[field[i][j]] = [i+1,j+1]
+
+for i in range(d+1,h*w+1):
+	distance[i] = distance[i-d] + abs(arr[i][0] - arr[i-d][0]) + abs(arr[i][1] - arr[i-d][1])
+
+
+for i in range(n):
+	l,r = inputs[i]
+
+	print distance[r]-distance[l]
