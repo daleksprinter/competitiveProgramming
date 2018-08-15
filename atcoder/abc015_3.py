6b8659c5431a160befbc0ef519ea8885cba1a6af
daleksprinter <b1015120@fun.ac.jp>
2018-08-15 16:30:50 +0900

    abc015_3.py

---
+++ atcoder/abc015_3.py
@@ -0,0 +1,18 @@
+n,m = map(int,raw_input().split())
+
+arr = []
+for i in range(n):
+	arr.append(map(int,raw_input().split()))
+
+def dfs(i,depth):
+	if depth == n:
+		if i == 0:
+			print 'Found'
+			quit()
+
+	else :
+		for j in range(m):
+			dfs(i ^ arr[depth][j],depth +1)
+dfs(0,0)
+print 'Nothing'
+
