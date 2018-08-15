3c908eac6d5bd435f9107ce9f57b23ec92911859
daleksprinter <b1015120@fun.ac.jp>
2018-08-15 13:11:02 +0900

    agc011_b.py

---
+++ atcoder/agc011_b.py
@@ -0,0 +1,16 @@
+import numpy as np
+n = int(input())
+
+arr = map(int,raw_input().split())
+arr.sort()
+cs = np.cumsum(arr)
+
+ans = 1
+
+i = n-2
+while i > -1 and cs[i] * 2 >= arr[i+1]:
+	ans += 1
+	i -= 1
+
+print ans
+
