1536bcea1b23be39936820870692a65385e89cdc
daleksprinter <b1015120@fun.ac.jp>
2018-08-16 14:27:19 +0900

    abc007_3.py

---
+++ atcoder/abc007_3.py
@@ -0,0 +1,30 @@
+h ,w = map(int,raw_input().split())
+
+sy, sx = map(int,raw_input().split())
+
+gy, gx = map(int,raw_input().split())
+
+arr = [raw_input() for i in range(h)]
+
+inf = float("inf")
+distance = [[inf for i in range(w)] for j in range(h)]
+distance[sy-1][sx-1] = 0
+queue = [[sy-1,sx-1,0]]
+direct = [[0,-1],[1,0],[0,1],[-1,0]]
+
+while queue :
+	ty, tx, cost = queue.pop(0)
+
+	if ty == gy-1 and tx == gx-1 :
+		print  distance[ty][tx]
+		quit()
+
+	for y,x in direct :
+		dy = ty + y
+		dx = tx + x
+
+		if -1 < dy < h and -1 < dx < w and arr[dy][dx] == '.' and distance[dy][dx] == inf :
+			distance[dy][dx] = cost + 1
+			queue.append([dy,dx,cost + 1])
+
+
