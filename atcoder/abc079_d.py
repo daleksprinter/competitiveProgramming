4bff1637125442c0e1b456412fa67e6f4b2a7d30
daleksprinter <b1015120@fun.ac.jp>
2018-08-17 04:57:42 +0900

    abc079_d.py

---
+++ atcoder/abc079_d.py
@@ -0,0 +1,10 @@
+h,w = map(int,raw_input().split())
+d = [map(int,raw_input().split()) for i in range(10)]
+for t in xrange(10):
+	for i in range(10):
+		for j in range(10):
+			for k in range(10):
+				d[i][j] = min(d[i][j], d[i][k] + d[k][j])
+arr = [map(int,raw_input().split()) for i in range(h)]
+
+print sum(sum(d[a][1] if a>=0 else 0 for a in aa) for aa in arr)
