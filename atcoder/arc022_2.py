b1d021bc8be64fee1e38ae10e60f2a4dfba13c86
daleksprinter <b1015120@fun.ac.jp>
2018-08-16 16:51:26 +0900

    arc022_2.py

---
+++ atcoder/arc022_2.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+arr = map(int,raw_input().split())
+
+l = 0
+r = 0
+ans = 0
+
+s = set()
+while r < n:
+	if not arr[r] in s:
+		s.add(arr[r])
+		r += 1
+		ans = max(ans, r-l+1)
+
+	else :
+		while arr[l] != arr[r]:
+			s.remove(arr[l])
+			l += 1
+		l += 1
+		r += 1
+
+
+print ans-1
