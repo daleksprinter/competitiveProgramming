cade532de2033f554644d232045dc319f7880b98
daleksprinter <b1015120@fun.ac.jp>
2018-08-17 04:10:36 +0900

    abc019_3.py

---
+++ atcoder/abc019_3.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+arr = map(int,raw_input().split())
+
+dic = {}
+
+for i in arr:
+	t = i
+	while t%2 == 0:
+		t /= 2
+
+	if t in dic :
+		dic[t] += 1
+
+	else :
+		dic[t] = 1
+
+print len(dic) 
