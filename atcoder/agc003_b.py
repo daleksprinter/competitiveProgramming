d46e34b0d49ef4bc56dd0898d13241949688b22c
daleksprinter <b1015120@fun.ac.jp>
2018-08-16 16:12:03 +0900

    agc003_b.py

---
+++ atcoder/agc003_b.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+arr = []
+
+for i in range(n):
+	arr.append(int(input()))
+
+l = 0
+r = 0
+
+
+ans = 0
+while l < n:
+	while  r+1 < n and arr[r+1] > 0:
+		r += 1
+
+	ans += sum(arr[l:r+1])/2
+	l = r+1
+	r = l
+
+print ans
+
+
