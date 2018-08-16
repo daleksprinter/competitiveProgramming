de9d705c4e66aae53c4e7f5c42c997f865dbf757
daleksprinter <b1015120@fun.ac.jp>
2018-08-17 03:54:42 +0900

    abc014_3.py

---
+++ atcoder/abc014_3.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+maxm = 10**6+1
+arr = [0 for i in range(maxm)]
+
+for i in range(n):
+	f,t = map(int,raw_input().split())
+
+	arr[f] += 1
+	if t + 1 < maxm : 
+		arr[t+1] -= 1
+
+import numpy
+cs = numpy.cumsum(arr)
+
+print max(cs)
+
