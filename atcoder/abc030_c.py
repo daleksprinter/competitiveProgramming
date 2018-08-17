50a821cb8756351f646087d5e5350a2d0cbd6652
daleksprinter <b1015120@fun.ac.jp>
2018-08-17 09:19:44 +0900

    abc030_c.py

---
+++ atcoder/abc030_c.py
@@ -0,0 +1,24 @@
+l = map(int,raw_input().split())
+
+cost = map(int,raw_input().split())
+
+arr = [map(int,raw_input().split()) for i in range(2)]
+
+ans = 0
+
+pos = [0,0]
+p = 0
+
+while True :
+
+	while pos[(p+1)%2] < l[(p+1)%2] and arr[p][pos[p]] + cost[p] > arr[(p+1)%2][pos[(p+1)%2]]:
+		pos[(p+1)%2] += 1
+
+	if p == 1 :
+		ans += 1
+
+	p = (p+1)%2
+
+	if pos[0] >= l[0] or pos[1] >= l[1]:
+		break
+print ans
