c2d99dcebe2b4eda05e2d8ecc9c79123e2c12056
daleksprinter <b1015120@fun.ac.jp>
2018-08-18 21:18:14 +0900

    abc106_c.py

---
+++ atcoder/abc106_c.py
@@ -0,0 +1,18 @@
+s = map(int,list(raw_input()))
+
+k = int(input())
+
+t = -1
+
+for i in range(len(s)):
+	if s[i] == 1:
+		t = i
+
+	else :
+		break
+
+if t > -1 and k-1 <= t:
+	print 1
+
+else :
+	print s[t+1]
