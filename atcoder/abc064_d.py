9fab604e1e992b5a919b4c0e99091fe0279ef94e
daleksprinter <b1015120@fun.ac.jp>
2018-08-21 09:52:02 +0900

    abc064_d.py

---
+++ atcoder/abc064_d.py
@@ -0,0 +1,24 @@
+
+n = int(input())
+
+s = list(raw_input())
+stk = []
+
+i = 0
+while i<len(s):
+	if s[i] == ')' and not stk:
+		s.insert(0,"(")
+		i += 1
+
+	if s[i] == ')' and  stk:
+		stk.pop()
+
+	if s[i] == '(':
+		stk.append('(')
+	i += 1
+
+for i in range(len(stk)):
+	stk[i] = ')'
+
+print ''.join(s) + ''.join(stk)
+
