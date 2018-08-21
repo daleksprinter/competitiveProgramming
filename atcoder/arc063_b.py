4aa8ce501b8731f28f8db07fbe6c9e4768dde724
daleksprinter <b1015120@fun.ac.jp>
2018-08-21 09:22:07 +0900

    arc063_b.py

---
+++ atcoder/arc063_b.py
@@ -0,0 +1,19 @@
+n,t = map(int,raw_input().split())
+
+arr = map(int,raw_input().split())
+
+mintable = []
+m = float("inf")
+for i in arr :
+	mintable.append(min(m,i))
+	m = min(m,i)
+
+ma = float("-inf")
+for i in range(n):
+	ma = max(ma,arr[i] - mintable[i])
+ans = 0
+for i in range(n):
+	if arr[i] - mintable[i] == ma:	
+		ans += 1
+
+print ans
