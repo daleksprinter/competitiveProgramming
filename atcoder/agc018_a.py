f1adbdaadea50fa96e5bbdd21f85b05cd0783bd3
daleksprinter <b1015120@fun.ac.jp>
2018-08-22 14:48:41 +0900

    agc018_a.py

---
+++ atcoder/agc018_a.py
@@ -0,0 +1,24 @@
+n,k = map(int,raw_input().split())
+
+arr = map(int,raw_input().split())
+if k > max(arr):
+	print "IMPOSSIBLE"
+	quit()
+
+def gcd(a,b):
+	if b == 0 :
+		return a
+	else :
+		return gcd(b,a%b)
+
+g = arr[0]
+
+for i in range(1,n):
+	g = gcd(g,arr[i])
+
+if k%g == 0 :
+	print "POSSIBLE"
+
+else :
+	print "IMPOSSIBLE"
+
