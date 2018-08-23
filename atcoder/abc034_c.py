f4908148552497db03edc8440016236c1cfb0837
daleksprinter <b1015120@fun.ac.jp>
2018-08-23 11:26:00 +0900

    abc034_c.py

---
+++ atcoder/abc034_c.py
@@ -0,0 +1,17 @@
+w,h = map(int,raw_input().split())
+
+import math
+mod = 10**9+7
+def inv(x):
+	return pow(x,mod-2,mod)
+
+def factorial(x):
+	retval = 1
+	for i in range(2,x+1):
+		retval = (retval * i)% mod
+	return retval
+
+def nCr(n,r):
+	return factorial(n) * inv(factorial(n-r)) * inv(factorial(r)) % mod
+
+print nCr(w+h-2,min(w-1,h-1))
