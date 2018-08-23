3417883d1d9f1044c4d0544b815c4bd2a95f42bb
daleksprinter <b1015120@fun.ac.jp>
2018-08-23 09:59:57 +0900

    code_festival_2017_qualc_c.py

---
+++ atcoder/code_festival_2017_qualc_c.py
@@ -0,0 +1,25 @@
+s = raw_input()
+
+l = 0
+r = len(s)-1
+
+ans = 0
+while l <= r and -1 < l < len(s) and -1 < r < len(s):
+	if s[l] == s[r]:
+		l += 1
+		r -= 1
+
+	else :
+		if s[l] == "x":
+			l += 1
+			ans += 1
+		elif s[r] == "x":
+			r -= 1
+			ans += 1
+
+		else :
+			print -1
+			quit()
+	
+print ans 
+
