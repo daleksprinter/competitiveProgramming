e1a9eed8309579f889c0721aaf15d21a3badc626
daleksprinter <b1015120@fun.ac.jp>
2018-08-24 17:34:04 +0900

    abc006_4.py

---
+++ atcoder/abc006_4.py
@@ -0,0 +1,12 @@
+import bisect
+n = int(input())
+inf = float("inf")
+arr = [int(input()) for _ in xrange(n)]
+
+dp = [inf for _ in xrange(n)]
+
+for i in arr:
+	ind = bisect.bisect_left(dp,i)
+	dp[ind] = i
+
+print n - bisect.bisect_left(dp,inf)
