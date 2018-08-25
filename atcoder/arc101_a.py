8278904baa88a4acf330984adde6e338d55764a2
daleksprinter <b1015120@fun.ac.jp>
2018-08-25 22:31:34 +0900

    arc101_a.py

---
+++ atcoder/arc101_a.py
@@ -0,0 +1,13 @@
+n,k = map(int,raw_input().split())
+
+arr = map(int,raw_input().split())
+
+ans = float("inf")
+import bisect
+t = bisect.bisect_right(arr,0)
+arr.insert(t,0)
+for i in range(n+1):
+	if i-k > -1:
+		ans = min(ans, abs(arr[i-k]) + abs(arr[i]) + min(abs(arr[i-k]), abs(arr[i])))
+
+print ans
