d01e23a89530e670f051eb881463862477152510
daleksprinter <b1015120@fun.ac.jp>
2018-08-27 23:09:53 +0900

    abc017_3.py

---
+++ atcoder/abc017_3.py
@@ -0,0 +1,16 @@
+import numpy as np
+n,m = map(int,raw_input().split())
+
+arr = [0] * m
+
+for i in range(n):
+	l,r,s = map(int,raw_input().split()); l-=1;r-= 1
+
+	if l > 0:
+		arr[0] += s
+		arr[l] -= s
+	if r < m-1:
+		arr[r+1] += s
+
+
+print max(np.cumsum(arr))
