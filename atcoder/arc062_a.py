a7d8b315040e295b1eaeb13a37b8806fff787b24
daleksprinter <b1015120@fun.ac.jp>
2018-08-28 16:30:05 +0900

    arc062_a.py

---
+++ atcoder/arc062_a.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+arr = [map(int,raw_input().split()) for i in range(n)]
+
+def ceil(n,d):
+ if n%d==0:
+  return n/d
+ else:
+  return n/d+1
+
+a = 1
+b = 1
+for i in range(n):
+	x, y = arr[i]
+	t = max(ceil(a,x), ceil(b,y))
+
+	a = x*t; b = y*t
+
+print a+b
