cf34a4a36601c9ea44978240ba5dfa81822c6c2d
daleksprinter <b1015120@fun.ac.jp>
2018-08-29 00:02:48 +0900

    abc084_c.py

---
+++ atcoder/abc084_c.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+arr = [map(int,raw_input().split()) for i in range(n-1)]
+
+def ceil(n,d):
+	if n%d==0:
+		return n/d
+
+	else:
+		return n/d+1
+
+for i in range(n):
+	t = 0
+
+	for j in range(i,n-1):
+		c,s,f = arr[j]
+		t = max(ceil(t,f)*f, s) + c
+
+
+	print t
+
