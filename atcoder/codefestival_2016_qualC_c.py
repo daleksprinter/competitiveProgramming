ba16882f1f747fe7cfcba9a14c64d11cc0d1e67e
daleksprinter <b1015120@fun.ac.jp>
2018-08-30 23:05:51 +0900

    codefestival_2016_qualC_c.py

---
+++ atcoder/codefestival_2016_qualC_c.py
@@ -0,0 +1,35 @@
+n = int(input())
+
+a = map(int,raw_input().split())
+b = map(int,raw_input().split())
+
+combs = [0 for i in range(n)]
+unique = [0 for i in range(n)]
+t = 0
+mod = 10**9 + 7
+for i in range(n):
+	if a[i] > t or a[i] == 1:
+		combs[i] = 1
+		t = a[i]
+		unique[i] = 1
+
+	else :
+		combs[i] = t
+t = 0
+for i in range(n-1,-1,-1):
+	if unique[i] == 1 and a[i] > b[i] :
+		print 0
+		quit()
+
+	if b[i] > t:
+		combs[i] = 1
+		t = b[i]
+
+	else :
+		combs[i] = min(combs[i], t)
+
+ans = 1
+for i in combs:
+	ans = (ans * i)%mod
+
+print ans
