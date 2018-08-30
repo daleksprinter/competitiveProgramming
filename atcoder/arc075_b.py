d3424191b265ef89dc357f81d91ed69a6c760f82
daleksprinter <b1015120@fun.ac.jp>
2018-08-30 17:55:07 +0900

    arc075_b.py

---
+++ atcoder/arc075_b.py
@@ -0,0 +1,34 @@
+n,a,b = map(int,raw_input().split())
+
+arr = []
+
+for i in range(n):
+	arr.append(int(input()))
+
+def ceil(a,b):
+	if a % b == 0:
+		return a/b
+	else :
+		return a/b + 1
+
+def istrue(x):
+	t = 0
+	for i in arr:
+		t += ceil((i - (b*x) if (i - (b*x)) > 0 else 0),(a-b))
+
+	if t <= x :
+		return True 
+
+	else :
+		return False
+
+
+left = 0; right = 10**9
+for i in range(30):
+	mid = (left + right)/2
+	if istrue(mid):
+		right = mid
+	else :
+		left = mid
+
+print right
