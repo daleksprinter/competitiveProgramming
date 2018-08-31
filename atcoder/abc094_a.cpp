c6ed91ef85a6aa385aa6144fda23cf5bfec2520f
daleksprinter <b1015120@fun.ac.jp>
2018-08-31 22:43:32 +0900

    abc094_a.cpp

---
+++ atcoder/abc094_a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,x;
+    cin >> a >> b >> x;
+    if(b >= x - a && a <= x){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
