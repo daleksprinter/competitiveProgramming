c5dc7307688d59d313530b6c514a370ea07c0113
daleksprinter <b1015120@fun.ac.jp>
2018-08-31 10:51:34 +0900

    abc005_3.py

---
+++ atcoder/abc005_3.py
@@ -0,0 +1,41 @@
+t = int(input())
+
+n = int(input())
+
+import Queue
+
+q = Queue.PriorityQueue()
+
+tmp = map(int,raw_input().split())
+
+for i in tmp:
+	q.put(i)
+
+m = int(input())
+
+arr = map(int,raw_input().split())
+
+i = 0
+while i < m:
+	issold = False
+	while not q.empty():
+		tako = q.get()
+
+		if tako > arr[i] :
+			print 'no'
+			quit()
+
+		if tako >= arr[i] - t :
+			issold = True
+			break
+	if issold :
+		i += 1
+
+	if q.empty() and i < m:
+		print 'no'
+		quit()
+
+	
+
+print 'yes'
+
