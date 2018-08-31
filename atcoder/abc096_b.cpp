6e547983cd802809887fc1e437bb8c124c856533
daleksprinter <b1015120@fun.ac.jp>
2018-08-31 22:59:31 +0900

    abc096_b.cpp

---
+++ atcoder/abc096_b.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int main(){
+    int a,b,c,n;
+    cin >> a >> b >> c;
+    cin >> n;
+
+    cout << pow(2,n) * max({a,b,c}) + a + b + c - max({a,b,c}) << endl;
+
+}
