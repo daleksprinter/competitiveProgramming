70aca5bf1b4db1649b3bd355c2e74a795b0a6f07
daleksprinter <b1015120@fun.ac.jp>
2018-08-31 23:15:34 +0900

    abc097_b.cpp

---
+++ atcoder/abc097_b.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int main(){
+    int x;
+    cin >> x;
+    int ans = 1;
+    for(int i = 2; i < x; i++){
+        int j =  2;
+        while (pow(i,j) <= x){
+            int t = pow(i,j);
+            ans = max({ans, t});
+            j++;
+        }
+    }
+    cout << ans << endl;
+}
