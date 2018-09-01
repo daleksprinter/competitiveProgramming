c9fbaaa8858d46375bfd7df0366d5960a98bd202
daleksprinter <b1015120@fun.ac.jp>
2018-09-01 12:09:38 +0900

    abc094_b.cpp

---
+++ atcoder/abc094_b.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <numeric>
+using namespace std;
+int main(){
+    int n,m,x;
+    cin >> n >> m >> x;
+    int i = m;
+    vector<int> arr;
+    while(i--){
+        int t;
+        cin >> t;
+        arr.push_back(t);
+    }
+    int count = 0;
+    for(int i = 0; i < m; i++){
+        if(arr[i] > x){
+            count++;
+        }
+    }
+    int count2 = 0;
+    for(int i = 0; i < m; i++){
+        if(arr[i] < x){
+            count2++;
+        }
+    }
+    cout << min({count,count2}) << endl;
+
+}
