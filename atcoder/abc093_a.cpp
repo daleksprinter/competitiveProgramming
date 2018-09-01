7c893cf76fc958f4c6a188b1d64af917212cf6b9
daleksprinter <b1015120@fun.ac.jp>
2018-09-01 12:23:21 +0900

    abc093_a.cpp

---
+++ atcoder/abc093_a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <numeric>
+#include <string>
+using namespace std;
+int main(){
+    string str;
+    cin >> str;
+    if(str.find("a") != string::npos && str.find("b") != string::npos && str.find("c") != string::npos){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
+
+}
+
