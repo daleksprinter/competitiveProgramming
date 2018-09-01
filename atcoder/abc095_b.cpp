aca3f7dd1a3e7680f0ad01119063bf774adea3b8
daleksprinter <b1015120@fun.ac.jp>
2018-09-01 12:02:43 +0900

    abc095_b.cpp

---
+++ atcoder/abc095_b.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <numeric>
+using namespace std;
+int main(){
+    int n, x;
+    cin >> n >> x;
+    vector<int> arr;
+    int i = n;
+    while (i--){
+        int t;
+        cin >> t;
+        arr.push_back(t);
+    }
+    cout << n + (x - accumulate(arr.begin(),arr.end(),0)) / *min_element(arr.begin(),arr.end()) << endl;
+}
+
