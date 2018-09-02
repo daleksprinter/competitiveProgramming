8d22383b8ca7e5bc065b2886b11c6faa73c8ed1c
daleksprinter <b1015120@fun.ac.jp>
2018-09-02 23:27:30 +0900

    abc077_b.cpp

---
+++ atcoder/abc077_b.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <math.h>
+#include <cmath>
+#include <limits.h>
+#include <map>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include <functional>
+#include <stdio.h>
+using namespace std;
+
+#define rep(i,a,b) for(int i=a;i<b;i++)
+
+const int inf = 1001001000;
+const int mod = 1000000007;
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
+
+int digitsum(int i){
+    int t = i;
+    int ret = 0;
+    while(t){
+        ret += t%10;
+        t /= 10;
+    }
+    return ret;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int i = 0;
+    while(i*i <= n)i++;
+    cout << (i-1) * (i-1) << endl; 
+}
+
+
