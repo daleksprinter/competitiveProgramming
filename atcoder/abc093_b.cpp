8c40f78c0f68e22290ea3516baf57a1d1ddcd952
daleksprinter <b1015120@fun.ac.jp>
2018-09-02 19:05:00 +0900

    abc093_b.cpp

---
+++ atcoder/abc093_b.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <numeric>
+#include <string>
+#include <set>
+using namespace std;
+
+#define rep(i,a,b) for(int i=a;i<b;i++)
+
+const int inf = 1001001000;
+const int mod = 1000000007;
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
+
+int main(){
+    int a, b, k;
+    cin >> a >> b >> k;
+
+    set<int> s;
+
+    rep(i, a,min(a+k,b)){
+        s.insert(i);
+    }
+
+    rep(i, max(a,b-k+1), b+1){
+        s.insert(i);
+    }
+
+    for(auto e: s) cout << e << endl;
+}
+
+
