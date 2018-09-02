d387f85b516ea9c88245394aae36e6aee82c2939
daleksprinter <b1015120@fun.ac.jp>
2018-09-02 20:52:07 +0900

    abc089_b.cpp

---
+++ atcoder/abc089_b.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <math.h>
+#include <cmath>
+#include <limits.h>
+#include <map>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include <functional>
+#include <stdio.h>
+using namespace std;
+
+#define rep(i,a,b) for(int i=a;i<b;i++)
+
+const int inf = 1001001000;
+const int mod = 1000000007;
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
+
+
+int main(){
+    int n;
+    cin >> n;
+    char c;
+    while(n--){ 
+        cin >> c; 
+        if(c == 'Y'){ 
+            cout << "Four" << endl;
+            return 0;
+        }
+    }
+    cout << "Three" << endl;
+    return 0;
+}
+
+
+
