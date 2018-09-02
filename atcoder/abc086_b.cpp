d9e651186cc49098f089b03617c205cbced77989
daleksprinter <b1015120@fun.ac.jp>
2018-09-02 21:32:54 +0900

    abc086_b.cpp

---
+++ atcoder/abc086_b.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <math.h>
+#include <cmath>
+#include <limits.h>
+#include <map>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include <functional>
+#include <stdio.h>
+using namespace std;
+
+#define rep(i,a,b) for(int i=a;i<b;i++)
+
+const int inf = 1001001000;
+const int mod = 1000000007;
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
+
+int digitlen(int i){
+    int t = i;
+    int count = 0;
+    while(t){
+        count++;
+        t /= 10;
+    }
+    return count;
+}
+
+int main(){
+    int a,b;
+    cin >> a >> b;
+    a = a * pow(10, digitlen(b)) + b;
+    for(int i = 0; i < sqrt(a)+1; i++){
+        if(a == i*i){
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+    cout << "No" << endl;
+}
+
+
