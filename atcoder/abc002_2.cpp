5f59e4b406f65f49551499d8dd76c65dc213c7b5
daleksprinter <b1015120@fun.ac.jp>
2018-09-04 11:29:38 +0900

    abc002_2.cpp

---
+++ atcoder/abc002_2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <math.h>
+#include <cmath>
+#include <limits.h>
+#include <map>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include <functional>
+#include <stdio.h>
+#include <ios>     // std::left, std::right
+#include <iomanip>
+using namespace std;
+
+#define rep(i,a,b) for(int i=a;i<b;i++)
+
+const int inf = 1001001000;
+//const int mod = 1000000007;
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
+
+
+
+
+
+int main(){
+ 
+    string s;
+    cin >> s;
+
+    string ans = "";
+
+    for(char e: s){
+        if(e == 'a' || e== 'i' || e == 'u' || e == 'e' || e == 'o'){
+            continue;
+        }else{
+            ans += e;
+        }
+    }
+
+    cout << ans << endl;
+
+}
