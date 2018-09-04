65efc0c7e7ff5de4259c721cbf5a04087b350aec
daleksprinter <b1015120@fun.ac.jp>
2018-09-04 11:48:21 +0900

    abc085_c.cpp

---
+++ atcoder/abc085_c.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <math.h>
+#include <cmath>
+#include <limits.h>
+#include <map>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include <functional>
+#include <stdio.h>
+#include <ios>     // std::left, std::right
+#include <iomanip>
+using namespace std;
+
+#define rep(i,a,b) for(int i=a;i<b;i++)
+
+const int inf = 1001001000;
+//const int mod = 1000000007;
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
+
+
+
+
+
+int main(){
+    int x,y;
+    cin >> x >> y;
+
+    rep(i,0,x+1){
+        rep(j,0,x-i+1){
+            if(10000*i + 5000*j + 1000*(x-i-j) == y){
+                cout << i << " " << j << " " << x-i-j << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "-1 -1 -1" << endl;
+}
+
+
