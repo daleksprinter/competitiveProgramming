161a686b4602834ef7ba789277635247ea7d47e1
daleksprinter <b1015120@fun.ac.jp>
2018-09-06 01:04:46 +0900

    abc028_d.cpp

---
+++ atcoder/abc028_d.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,a,b) for(int i=a;i<b;i++)
+typedef long long ll;
+
+int main(){
+    double n, k;
+    cin >> n >> k;
+    cout << fixed << setprecision(10) <<((k-1)*(n-k)*6 + (k-1)*3 + (n-k)*3 + 1)/n/n/n << endl;;
+}
