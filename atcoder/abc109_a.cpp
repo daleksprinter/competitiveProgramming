4987adef01ed71359005f5cf32b8fa45f45a17ec
daleksprinter <b1015120@fun.ac.jp>
2018-09-08 21:02:58 +0900

    abc109_a.cpp

---
+++ atcoder/abc109_a.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,a,b) for(int i=a;i<b;i++)
+typedef long long ll;
+const ll inf = 100100100100100;
+
+
+int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    int a,b;
+    cin >> a >> b;
+    if(a*b %2 == 1){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
+
+}
