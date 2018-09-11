f7cf467aa53212a3723fb5ec8baa970d7641782b
daleksprinter <b1015120@fun.ac.jp>
2018-09-11 18:23:55 +0900

    abc057_c.cpp

---
+++ atcoder/abc057_c.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define int long long
+const int inf = 100100100100100;
+const int mod = 1000000007;
+
+int digitlen(int i){
+    int t = i;
+    int count = 0;
+    while(t){
+        count += 1;
+        t /= 10;
+    }
+    return count;
+}
+
+signed main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    int n;
+    cin >> n;
+    int ans = 100;
+    rep(i,1,(int)sqrt(n)+1){
+        if(n%i == 0){
+            ans = min(ans, digitlen(n/i));
+        }
+    }
+    cout << ans << endl;
+}
