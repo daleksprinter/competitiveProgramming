b155b8829f4cc24085ca6b0b0ca6a518e36ae136
daleksprinter <b1015120@fun.ac.jp>
2018-09-20 10:52:23 +0900

    arc098_b.cpp

---
+++ atcoder/arc098_b.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define int long long
+const int inf = 100100100100100009;
+const int mod = 1000000007;
+
+signed main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    int n; cin >> n;
+    int arr[n]; rep(i,0,n) cin >> arr[i];
+
+    int buff = 0;
+    int left = 0;
+    int right = 0;
+    int ans = 0;
+
+    while(left < n){
+
+        if(right < n && (buff ^ arr[right]) == buff + arr[right]){
+            buff += arr[right];
+            right++;
+            
+        }else{
+            ans += right - left;
+            buff -= arr[left];
+            left++;
+        }
+    }
+    
+    cout << ans << endl;
+    
+}   
