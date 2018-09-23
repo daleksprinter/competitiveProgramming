3c288bc6f6196107412bb78266b2ffdde0ea0c91
daleksprinter <b1015120@fun.ac.jp>
2018-09-23 21:32:43 +0900

    abc110_c.cpp

--- atcoder/abc110_c.cpp
+++ atcoder/abc110_c.cpp
@@ -1,104 +1,69 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
 
-/* macro */
+//macro-----------------------------------------------------------------------------------------
 #define rep(i,a,b) for(int i=a;i<b;i++)
-#define revrep(i,a,b) for(int i = a; i > b; i--)
+#define revrep(i,a,b) for(int i=a;i>b;i--)
 #define int long long
-#define exist(s,e) ((s).find(e)!=(s).end())
-#define all(v) (v).begin(), (v).end()
-#define each(s,itr) for(auto (itr) = s.begin(); (itr) != s.end(); (itr)++)
-#define sum(v) accumulate(all(v), (0LL))
-#define isin(a, b, c) (b <= a && a <= c)
-#define println cout << "\n";
-#define sz(v) (int)v.size()
-#define bin(x) static_cast<bitset<16> >(x)
 
+//constant--------------------------------------------------------------------------------------
+const int inf = 100100100100000;
+const int mod = 1000000007;
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
 
+//general_method--------------------------------------------------------------------------------
+vector<int> cs(vector<int> arr){vector<int> ret = arr;rep(i,1,arr.size()){ret[i] += ret[i-1];}return ret;}
+int ceil(int a, int b){if(a%b == 0) return a/b;else return a/b + 1;}
+int digitlen(int i){int t = i;int count = 0;while(t){count += 1;t /= 10;}return count;}
+int gcd(int m, int n){if((0== m)||(0 == n))return 0;while(m != n){if(m > n)m = m - n;else n = n - m;}return m;}
+int lcm(int n, int m){return n*m/gcd(n,m);}
 
-/* alias */
-template<class T> using vec = vector<T>;
-typedef vector<int> vi;
-typedef pair<int, int> pi;
-typedef tuple<int, int, int> ti;
-typedef map<int, int> mi;
-typedef set<int> si;
-
-
-/* constant */
-const int inf = 1LL << 62;
-const int mod = 1e9 + 7;
-const int dx[8]={1,0,-1,0,-1,1,-1,1};
-const int dy[8]={0,1,0,-1,-1,-1,1,1};
-const string alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-/* io_method */
+//io_method-------------------------------------------------------------------------------------
 int input(){int tmp;cin >> tmp;return tmp;}
 string raw_input(){string tmp;cin >> tmp;return tmp;}
-template<class T> void printx(T n){cout << n;}
-template<class T, class U> void printx(pair<T, U> p){cout << "(" << p.first << "," << p.second << ")";}
-template<class T, class U, class V> void printx(tuple<T, U, V> t){cout << "{" << get<0>(t) << "," << get<1>(t) <<"," << get<2>(t) << "}" << endl;}
-template<class T> void printx(vector<T> v){cout << "{";rep(i,0,v.size()){printx(v[i]);if(i != v.size()-1)printx(",");}cout << "}";}
-template<class T> void print(T n){printx(n);cout << endl;}
-template<class T> void print(set<T> s){cout << "{";each(s, e){if(e != s.begin()) printx(",");printx(*e);}cout << "}" << endl;}
-template<class T, class U> void print(map<T, U> mp){cout << "{";each(mp, e){cout << "(" << e -> first << "," << e -> second << ")";}cout << "}" << endl;}
-template<class T> void printans(vec<T> v){rep(i,0,sz(v)){cout << v[i] << (i == sz(v) - 1 ? "" : " ");}cout << endl;}
+string readline(){string s;getline(cin, s);return s;}
+void print(int n){cout << n << endl;}
+void print(vector<int> arr){cout << arr[0];rep(i,1,arr.size()) cout << " " << arr[i];cout << endl;}
+void print(string s){cout << s << endl;}
+void print(pair<int, int> p){cout << p.first << " " << p.second;}
+void print(vector<vector<int>> arr){rep(i,0,arr.size()){print(arr[i]);}}
 
+//main------------------------------------------------------------------------------------------
 
-/* general_method */
-template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
-template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
-template<class T>T cut(T &a, int l, int r){return T(a.begin()+l, a.begin()+r);}
-
-/* main */
-
-string s;
-string t;
-void in(){
-    cin >> s >> t;
-}
-
-void solve(){
-    
-    map<char, char> mp;
-    map<char, char> mp2;
-
-    bool isok = true;
-    rep(i,0,sz(s)){
-
-
-        if(exist(mp, s[i])){
-            if(mp[s[i]] != t[i]){
-                isok = false;
-                break;
-            }
-        }else{
-            mp[s[i]] = t[i];
-        }
-
-    }
-    rep(i,0,sz(s)){
-
-        if(exist(mp2, t[i])){
-            if(mp2[t[i]] != s[i]){
-                isok = false;
-                break;
-            }
-        }else{
-            mp2[t[i]] = s[i];
-        }
-    }
-
-    print(isok ? "Yes" : "No");
-
-
-}
 
 signed main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
 
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-    in();
-    solve();
     
+    string s = raw_input();
+    string t = raw_input();
+
+    map<char, char> mp;
+    rep(i,0,s.size()){
+        if(mp.find(t[i]) == mp.end()){
+            mp[t[i]] = s[i];
+        }else{
+            if(mp[t[i]] != s[i]){
+                print("No");
+                return 0;
+            }
+        }
+    }
+
+    map<char, char> mp2;
+    rep(i,0,s.size()){
+        if(mp2.find(s[i]) == mp2.end()){
+            mp2[s[i]] = t[i];
+        }else{
+            if(mp2[s[i]] != t[i]){
+                print("No");
+                return 0;
+            }
+        }
+    }
+
+    print("Yes");
+
 }
