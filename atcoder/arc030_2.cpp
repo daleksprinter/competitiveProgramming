507d6481c2fa264bb079a0af70dc78f138c58fd6
daleksprinter <b1015120@fun.ac.jp>
2018-09-27 11:57:08 +0900

    arc030_2.cpp

---
+++ atcoder/arc030_2.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//macro-----------------------------------------------------------------------------------------
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i=a;i>b;i--)
+#define int long long
+
+//constant--------------------------------------------------------------------------------------
+const int inf = 100100100100000;
+const int mod = 1000000007;
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
+
+//general_method--------------------------------------------------------------------------------
+vector<int> cs(vector<int> arr){vector<int> ret = arr;rep(i,1,arr.size()){ret[i] += ret[i-1];}return ret;}
+int ceil(int a, int b){if(a%b == 0) return a/b;else return a/b + 1;}
+int digitlen(int i){int t = i;int count = 0;while(t){count += 1;t /= 10;}return count;}
+int gcd(int m, int n){if((0== m)||(0 == n))return 0;while(m != n){if(m > n)m = m - n;else n = n - m;}return m;}
+int lcm(int n, int m){return n*m/gcd(n,m);}
+
+//io_method-------------------------------------------------------------------------------------
+int input(){int tmp;cin >> tmp;return tmp;}
+string raw_input(){string tmp;cin >> tmp;return tmp;}
+string readline(){string s;getline(cin, s);return s;}
+void print(int n){cout << n << endl;}
+void print(vector<int> arr){cout << arr[0];rep(i,1,arr.size()) cout << " " << arr[i];cout << endl;}
+void print(string s){cout << s << endl;}
+void print(pair<int, int> p){cout << p.first << " " << p.second;}
+void print(vector<vector<int>> arr){rep(i,0,arr.size()){print(arr[i]);}}
+
+//main------------------------------------------------------------------------------------------
+
+
+
+vector<int> arr(100); 
+vector<bool> visit(100, false);
+vector<bool> used(100, false);
+vector<vector<int>> edges(100);
+
+bool dfs(int v){
+    bool ret = false;
+    used[v] = true;
+    if(arr[v] == 1) ret = true;
+
+    rep(i,0,edges[v].size()){
+        if(not used[edges[v][i]] and dfs(edges[v][i])) ret = true;
+    }
+
+    visit[v] = ret;
+    return ret;
+}   
+
+
+
+signed main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    int n, x; cin >> n >> x;
+
+    rep(i,0,n) arr[i] = input();
+
+    rep(i,0,n-1){
+        int f, t; cin >> f >> t;
+        f--; t--;
+        edges[f].push_back(t);
+        edges[t].push_back(f);
+    }
+
+    dfs(x-1);
+
+    int ans = (count(visit.begin(), visit.begin() + n, true)-1) * 2;
+    print(ans > 0 ? ans : 0);
+    
+
+
+    
+
+
+}
