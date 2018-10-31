54fade6e450b5b20f70005bd52b724204190e28f
daleksprinter <b1015120@fun.ac.jp>
2018-10-31 22:50:01 +0900

    abc023_c.cpp

---
+++ atcoder/abc023_c.cpp
@@ -0,0 +1,171 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+
+/* macro */
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i = a; i > b; i--)
+#define int long long
+#define exist(s,e) ((s).find(e)!=(s).end())
+#define all(v) (v).begin(), (v).end()
+#define each(s,itr) for(auto (itr) = s.begin(); (itr) != s.end(); (itr)++)
+#define sum(v) accumulate(all(v), (0LL))
+#define mp(a, b) make_pair(a, b)
+
+
+/* alias */
+template<class T> using vec = vector<T>;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+
+/* constant */
+const int inf = 100100100100100100;
+const int mod = 1e9;
+int dx[8]={1,0,-1,0,-1,1,-1,1};
+int dy[8]={0,1,0,-1,-1,-1,1,1};
+string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+
+/* io_method */
+int input(){int tmp;cin >> tmp;return tmp;}
+string raw_input(){string tmp;cin >> tmp;return tmp;}
+string readline(){string s;getline(cin, s);return s;}
+template<class T> void printx(T n){cout << n;}
+template<class T, class U> void printx(pair<T, U> p){cout << "(" << p.first << "," << p.second << ")";}
+template<class T> void printx(vector<T> v){cout << "{";rep(i,0,v.size()){printx(v[i]);if(i != v.size()-1)printx(",");}cout << "}";}
+template<class T> void print(T n){printx(n);cout << endl;}
+template<class T> void print(set<T> s){cout << "{";each(s, e){if(e != s.begin()) printx(",");printx(*e);}cout << "}" << endl;}
+template<class T, class U> void print(map<T, U> mp){cout << "{";each(mp, e){cout << "(" << e -> first << "," << e -> second << ")";}cout << "}" << endl;}
+
+
+/* general_method */
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+
+
+
+/* main */
+
+
+
+
+signed main(){
+
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    
+    int h, w, k;
+    cin >> h >> w >> k;
+
+    int n = input();
+
+    vi ax(w, 0);
+    vi ay(h, 0);
+
+    vec<pi> arr;
+    rep(i,0,n){
+        int y, x; cin >> y >> x;
+        y--; x--;
+        ay[y]++; ax[x]++;
+        arr.push_back(mp(y, x));
+    }
+
+    vi sorteday = ay;
+    sort(all(sorteday));
+
+    int cnt = 0;
+    rep(i,0,w){
+        cnt += distance(lower_bound(all(sorteday), k - ax[i]), upper_bound(all(sorteday), k - ax[i]));
+    }
+
+    rep(i,0,n){
+        int y = arr[i].first; int x = arr[i].second;
+        if(ax[x] + ay[y] == k) cnt--;
+        if(ax[x] + ay[y] == k + 1) cnt++;
+    }
+    print(cnt);
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+    
+    
+
+
+
+    
+    
+
+
+
+ 
+    
+    
+
+
+    
+
+    
+   
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
