cdfa0983e4701feb9dddb82974920701b24e7ccb
daleksprinter <b1015120@fun.ac.jp>
2018-11-01 02:24:02 +0900

    arc061_b.cpp

---
+++ atcoder/arc061_b.cpp
@@ -0,0 +1,156 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+
+/* macro */
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i = a; i > b; i--)
+#define int long long
+#define exist(s,e) ((s).find(e)!=(s).end())
+#define all(v) (v).begin(), (v).end()
+#define each(s,itr) for(auto (itr) = s.begin(); (itr) != s.end(); (itr)++)
+#define sum(v) accumulate(all(v), (0LL))
+
+
+/* alias */
+template<class T> using vec = vector<T>;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+
+/* constant */
+const int inf = 100100100100100100;
+const int mod = 1e9 + 7;
+int dx[8]={1,0,-1,0,-1,1,-1,1};
+int dy[8]={0,1,0,-1,-1,-1,1,1};
+
+
+/* io_method */
+int input(){int tmp;cin >> tmp;return tmp;}
+string raw_input(){string tmp;cin >> tmp;return tmp;}
+string readline(){string s;getline(cin, s);return s;}
+template<class T> void printx(T n){cout << n;}
+template<class T, class U> void printx(pair<T, U> p){cout << "(" << p.first << "," << p.second << ")";}
+template<class T> void printx(vector<T> v){cout << "{";rep(i,0,v.size()){printx(v[i]);if(i != v.size()-1)printx(",");}cout << "}";}
+template<class T> void print(T n){printx(n);cout << endl;}
+template<class T> void print(set<T> s){cout << "{";each(s, e){if(e != s.begin()) printx(",");printx(*e);}cout << "}" << endl;}
+template<class T, class U> void print(map<T, U> mp){cout << "{";each(mp, e){cout << "(" << e -> first << "," << e -> second << ")";}cout << "}" << endl;}
+
+
+/* general_method */
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+
+
+
+/* main */
+
+
+signed main(){
+
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int h, w, n; cin >> h >> w >> n;
+
+    map<pi, int> mp;
+
+    rep(k,0,n){
+        int y, x; cin >> y >> x;
+
+        rep(i,-1, 2){
+            rep(j,-1, 2){
+                int dx = x + j; int dy = y + i;
+                mp[make_pair(dy, dx)]++;
+            }
+        }
+    }
+
+
+
+    int ans[10]; memset(ans, 0, sizeof(ans));
+
+    int z = 0;
+    each(mp, itr){
+        pi cell = itr -> first;
+        if(1 < cell.first && cell.first < h && 1 < cell.second && cell.second < w){
+            ans[mp[cell]]++;
+            z++;
+        }
+    }
+
+
+    print((w - 2) * (h - 2) - z);
+    rep(i,1,10){
+        print(ans[i]);
+    }
+
+
+
+    
+
+
+
+    
+
+
+
+    
+
+ 
+    
+    
+
+
+    
+
+    
+   
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
