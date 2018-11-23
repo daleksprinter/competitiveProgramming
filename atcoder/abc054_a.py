af776b98a13a0996af9b30fb66dcfd0089c241c4
daleksprinter <b1015120@fun.ac.jp>
2018-11-23 12:52:13 +0900

    abc054_a.py

---
+++ atcoder/abc054_a.py
@@ -0,0 +1,11 @@
+a = [2,3,4,5,6,7,8,9,10,11, 12,13, 1]
+
+alice, bob = map(int, raw_input().split())
+
+if a.index(alice) > a.index(bob):
+	print "Alice"
+
+elif a.index(alice) < a.index(bob):
+	print "Bob"
+else:
+	print "Draw"
