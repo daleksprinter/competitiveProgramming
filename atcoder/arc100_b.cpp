5fb8298154fb2e0094b7b1bafbc1b27595e9fc6c
daleksprinter <b1015120@fun.ac.jp>
2019-01-04 05:18:54 +0900

    arc100_b.cpp

---
+++ atcoder/arc100_b.cpp
@@ -0,0 +1,172 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+
+/* macro */
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i = a; i > b; i--)
+#define int long long
+#define exist(s,e) ((s).find(e)!=(s).end())
+#define all(v) (v).begin(), (v).end()
+#define each(s,itr) for(auto (itr) = s.begin(); (itr) != s.end(); (itr)++)
+#define sum(v) accumulate(all(v), (0LL))
+#define pb push_back
+
+
+/* alias */
+template<class T> using vec = vector<T>;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+
+/* constant */
+const int inf = pow(2, 62);
+const int mod = 1e9 + 7;
+int dx[8]={1,0,-1,0,-1,1,-1,1};
+int dy[8]={0,1,0,-1,-1,-1,1,1};
+const string alpha = "abcdefghijklmnopqrstuvwxyz";
+
+/* io_method */
+int input(){int tmp;cin >> tmp;return tmp;}
+string raw_input(){string tmp;cin >> tmp;return tmp;}
+string readline(){string s;getline(cin, s);return s;}
+template<class T> void printx(T n){cout << n;}
+template<class T, class U> void printx(pair<T, U> p){cout << "(" << p.first << "," << p.second << ")";}
+template<class T> void printx(vector<T> v){cout << "{";rep(i,0,v.size()){printx(v[i]);if(i != v.size()-1)printx(",");}cout << "}";}
+template<class T> void print(T n){printx(n);cout << endl;}
+template<class T> void print(set<T> s){cout << "{";each(s, e){if(e != s.begin()) printx(",");printx(*e);}cout << "}" << endl;}
+template<class T, class U> void print(map<T, U> mp){cout << "{";each(mp, e){cout << "(" << e -> first << "," << e -> second << ")";}cout << "}" << endl;}
+template<class T> void printans(vector<T> v){
+    rep(i,0,v.size() - 1){
+        cout << v[i] + 1 << " ";
+    }
+    cout << v[v.size() - 1] + 1 << endl;
+}
+
+/* general_method */
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+template<typename T>
+auto cut(T &a, int l, int r){
+    return T(a.begin()+l, a.begin()+r);   
+}
+
+
+/* main */
+
+struct cumsum1d{
+    int n;
+    vector<int> arr;
+
+    cumsum1d(int len){
+        n = len;
+        rep(i,0,n) arr.push_back(0);
+    }
+
+    void init(vector<int> vec){
+        arr = vec;
+        n = arr.size();
+    }
+
+    void add(int a, int b, int val = 1){
+        arr[a] += val;
+        if(b + 1 < n) arr[b + 1] -= val;
+    }
+
+    void build(){
+        rep(i,1,n) arr[i] += arr[i-1];
+    }
+
+    int query(int from, int to){
+        int ret = arr[to];
+        if(from) ret -= arr[from - 1];
+        return ret;
+    }
+
+};
+
+signed main(){
+
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n = input();
+    vi arr; rep(i,0,n) arr.push_back(input());
+
+    cumsum1d cs = cumsum1d(1); cs.init(arr); cs.build();
+
+    int l = 0, r = 2;
+    int ans = inf;
+    rep(i,1, n - 2){
+        while(l < i - 1 && abs(cs.arr[i] - 2 * cs.query(0, l)) > abs(cs.arr[i] - 2 * cs.query(0, l + 1)))  l++;
+        while(r < n - 2 && abs(cs.query(i + 1, n - 1) - 2 * cs.query(i + 1, r)) > abs(cs.query(i + 1, n - 1) - 2 * cs.query(i + 1, r + 1))) r++;
+
+        vi hoge = {cs.query(0, l), cs.query(l + 1, i), cs.query(i + 1, r), cs.query(r + 1, n - 1)};
+
+        sort(all(hoge));
+        
+        chmin(ans, hoge.back() - hoge.front());
+
+
+    }
+    print(ans);
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+    
+
+}
+
+
