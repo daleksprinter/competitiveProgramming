9b5212b07aa278fb3319d98b33e01b55de4f785b
daleksprinter <b1015120@fun.ac.jp>
2019-01-04 07:47:57 +0900

    arc077_b.cpp

---
+++ atcoder/arc077_b.cpp
@@ -0,0 +1,224 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+
+/* macro */
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i = a; i > b; i--)
+#define int long long
+#define exist(s,e) ((s).find(e)!=(s).end())
+#define all(v) (v).begin(), (v).end()
+#define each(s,itr) for(auto (itr) = s.begin(); (itr) != s.end(); (itr)++)
+#define sum(v) accumulate(all(v), (0LL))
+#define pb push_back
+
+
+/* alias */
+template<class T> using vec = vector<T>;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+
+/* constant */
+const int inf = pow(2, 62);
+const int mod = 1e9 + 7;
+int dx[8]={1,0,-1,0,-1,1,-1,1};
+int dy[8]={0,1,0,-1,-1,-1,1,1};
+const string alpha = "abcdefghijklmnopqrstuvwxyz";
+
+/* io_method */
+int input(){int tmp;cin >> tmp;return tmp;}
+string raw_input(){string tmp;cin >> tmp;return tmp;}
+string readline(){string s;getline(cin, s);return s;}
+template<class T> void printx(T n){cout << n;}
+template<class T, class U> void printx(pair<T, U> p){cout << "(" << p.first << "," << p.second << ")";}
+template<class T> void printx(vector<T> v){cout << "{";rep(i,0,v.size()){printx(v[i]);if(i != v.size()-1)printx(",");}cout << "}";}
+template<class T> void print(T n){printx(n);cout << endl;}
+template<class T> void print(set<T> s){cout << "{";each(s, e){if(e != s.begin()) printx(",");printx(*e);}cout << "}" << endl;}
+template<class T, class U> void print(map<T, U> mp){cout << "{";each(mp, e){cout << "(" << e -> first << "," << e -> second << ")";}cout << "}" << endl;}
+template<class T> void printans(vector<T> v){
+    rep(i,0,v.size() - 1){
+        cout << v[i] + 1 << " ";
+    }
+    cout << v[v.size() - 1] + 1 << endl;
+}
+
+/* general_method */
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+template<typename T>
+auto cut(T &a, int l, int r){
+    return T(a.begin()+l, a.begin()+r);   
+}
+
+
+/* main */
+
+struct binomial_coefficient{
+    vector<int> facts;
+    vector<int> invs;
+    
+    int mod_pow(int x, int n, int m){
+        if(n== 0) return 1;
+        int res = mod_pow(x*x%m,n/2,m);
+        if(n&1) res = res*x%m;
+        return res;
+    }
+
+    int factorial(int x){
+        int res = 1;
+        rep(i,2,x+1){
+            res *= i;res %= mod;
+        }
+        return res;
+    }
+    
+    int inv(int n){
+        return mod_pow(n,mod-2,mod);
+    }
+
+    void init(){
+        facts.push_back(1); invs.push_back(1);
+        for(int i = 1; i < 300005; i++){
+            facts.push_back(facts[i-1]*i%mod);
+            invs.push_back(inv(facts[i]));
+        }
+        
+    }
+
+    int nPr(int n, int r){
+        if(r > n) return 0;
+        return (facts[n]%mod * invs[n-r]% mod)%mod;
+    }
+
+    int nCr(int n, int r){
+
+        if(r > n) return 0;
+
+        if(r < 0) return 0;
+
+        int ans = facts[n]%mod * invs[n-r]%mod * invs[r] % mod;
+
+
+        return ans;
+    }
+
+    int nHr(int n, int r){
+        if(n==0 && r==0) return 1;
+        return nCr(n+r-1, r);
+    }
+
+};
+
+signed main(){
+
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n = input();
+
+    vi arr; rep(i,0,n + 1) arr.push_back(input());
+
+    set<int> s;
+    int r;
+    int l;
+    rep(i,0,n + 1){
+        if(not exist(s, arr[i])){
+            s.insert(arr[i]);
+
+        }else{
+            r = i;
+            l = arr[r];
+            break;
+        }
+    }
+
+    rep(i,0,n + 1){
+        if(arr[i] == l){
+            l = i;
+            break;
+        }
+    }
+
+
+
+    r = n + 1 - (r + 1);
+
+
+    binomial_coefficient bf = binomial_coefficient();
+    bf.init();
+
+    rep(i,1,n + 2){
+
+        print(((bf.nCr(n + 1, i) - bf.nCr(l + r, i - 1) + mod))  % mod);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+    
+
+}
+
+
