e871d469fd9a3f00c9181ebdbcc0a389feac047a
daleksprinter <b1015120@fun.ac.jp>
2019-01-08 05:43:08 +0900

    dp_h.cpp

---
+++ atcoder/dp_h.cpp
@@ -0,0 +1,149 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+/* macro */
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i = a; i > b; i--)
+#define int long long
+#define exist(s,e) ((s).find(e)!=(s).end())
+#define all(v) (v).begin(), (v).end()
+#define each(s,itr) for(auto (itr) = s.begin(); (itr) != s.end(); (itr)++)
+#define sum(v) accumulate(all(v), (0LL))
+
+
+
+/* alias */
+
+
+template<class T> using vec = vector<T>;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+
+/* constant */
+const int inf = 10001000100;
+const int mod = 1e9 + 7;
+int dx[8]={1,0,-1,0,-1,1,-1,1};
+int dy[8]={0,1,0,-1,-1,-1,1,1};
+
+
+/* io_method */
+int input(){int tmp;cin >> tmp;return tmp;}
+string raw_input(){string tmp;cin >> tmp;return tmp;}
+string readline(){string s;getline(cin, s);return s;}
+template<class T> void printx(T n){cout << n;}
+template<class T, class U> void printx(pair<T, U> p){cout << "(" << p.first << "," << p.second << ")";}
+template<class T> void printx(vector<T> v){cout << "{";rep(i,0,v.size()){printx(v[i]);if(i != v.size()-1)printx(",");}cout << "}";}
+template<class T> void print(T n){printx(n);cout << endl;}
+template<class T> void print(set<T> s){cout << "{";each(s, e){if(e != s.begin()) printx(",");printx(*e);}cout << "}" << endl;}
+template<class T, class U> void print(map<T, U> mp){cout << "{";each(mp, e){cout << "(" << e -> first << "," << e -> second << ")";}cout << "}" << endl;}
+
+
+/* general_method */
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+
+/* main */
+
+
+signed main(){
+
+
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+
+    int h, w;
+    cin >> h >> w;
+
+
+	char arr[1000][1000];
+
+	rep(i,0,h){
+		rep(j,0,w){
+			char tmp; cin >> tmp;
+			arr[i][j] = tmp;
+		}
+	}
+
+	int dp[1000][1000];
+
+	dp[0][0] = 1;
+
+	rep(i,0,h){
+		rep(j,0,w){
+			if(i == 0 && j == 0) continue;
+
+			if(arr[i][j] == '#'){
+				dp[i][j] = 0;
+				continue;
+			}
+
+			if(i == 0){
+				dp[0][j] = dp[0][j - 1];
+				continue;
+			}
+
+			if(j == 0){
+				dp[i][0] = dp[i - 1][0];
+				continue;
+			}
+
+			dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % mod;
+
+		}
+	}
+
+	print(dp[h - 1][w - 1] % mod);
+
+
+	
+
+
+
+	
+	
+
+
+	
+
+
+
+
+	
+
+
+
+
+    
+
+
+
+	
+
+
+
+
+
+
+
+
+    
+
+
+
+    
+
+
+
+
+
+    
+
+
+    
+
+}
+
+
