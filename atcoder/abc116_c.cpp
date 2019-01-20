e22c7db43735124197df442259ad84a7fc72ca5a
daleksprinter <b1015120@fun.ac.jp>
2019-01-20 21:22:44 +0900

    abc116_c.cpp

--- atcoder/abc116_c.cpp
+++ atcoder/abc116_c.cpp
@@ -1,35 +1,77 @@
-#include <cstdio>
-#include <iostream>
-#include <vector>
+#include "bits/stdc++.h"
+using namespace std;
 
-int main() {
-  using namespace std;
+/* macro */
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i = a; i > b; i--)
+#define int long long
+#define exist(s,e) ((s).find(e)!=(s).end())
+#define all(v) (v).begin(), (v).end()
+#define each(s,itr) for(auto (itr) = s.begin(); (itr) != s.end(); (itr)++)
+#define sum(v) accumulate(all(v), (0LL))
+#define isin(tx, ty, w, h) (0 <= tx && tx <= w && 0 <= ty && ty <= h)
 
-  int n;
-  scanf("%d", &n);
+/* alias */
+template<class T> using vec = vector<T>;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
 
-  vector<int> h(n);
-  for (int i = 0; i < n; i++) {
-    scanf("%d", &h[i]);
-  }
+/* constant */
+const int inf = pow(2, 62);
+const int mod = 1e9 + 7;
+int dx[8]={1,0,-1,0,-1,1,-1,1};
+int dy[8]={0,1,0,-1,-1,-1,1,1};
 
-  int watering_times = 0;
-  for (int i = 0; i < n; i++) {
-    while(h[i]) {
-      watering_times++;
-      h[i]--;
-      int j = i+1;
-      if(j == n) continue;
-      while(h[j]) {
-        h[j]--;
-        j++;
-        if(j == n) break;
-      }
-    }
-  }
+/* io_method */
+int input(){int tmp;cin >> tmp;return tmp;}
+string raw_input(){string tmp;cin >> tmp;return tmp;}
+template<class T> void printx(T n){cout << n;}
+template<class T, class U> void printx(pair<T, U> p){cout << "(" << p.first << "," << p.second << ")";}
+template<class T> void printx(vector<T> v){cout << "{";rep(i,0,v.size()){printx(v[i]);if(i != v.size()-1)printx(",");}cout << "}";}
+template<class T> void print(T n){printx(n);cout << endl;}
+template<class T> void print(set<T> s){cout << "{";each(s, e){if(e != s.begin()) printx(",");printx(*e);}cout << "}" << endl;}
+template<class T, class U> void print(map<T, U> mp){cout << "{";each(mp, e){cout << "(" << e -> first << "," << e -> second << ")";}cout << "}" << endl;}
 
-  cout << watering_times << endl;
+/* general_method */
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+template<class T>T cut(T &a, int l, int r){return T(a.begin()+l, a.begin()+r);}
 
-  return 0;
+/* main */
+
+int n;
+vi arr;
+
+void in(){
+    cin >> n;
+    rep(i,0,n) arr.push_back(input());
 }
 
+void solve(){
+
+    int ans = 0;
+    while(true){
+        int l = 0;
+        while(l < n && arr[l] == 0) l++;
+
+        if(l == n) break;
+        int r = l;
+        while(r + 1 < n && arr[r + 1] != 0) r++;
+
+        rep(i,l,r + 1){
+            arr[i]--;
+        }
+        ans++;
+    }
+    print(ans);
+
+}
+
+signed main(){
+
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    in();
+    solve();
+    
+}
