f7bbe160643ec61e2f1399556b7ccb4fc81b14a9
daleksprinter <b1015120@fun.ac.jp>
2019-01-26 13:02:35 +0900

    abc013_3.cpp

---
+++ atcoder/abc013_3.cpp
@@ -0,0 +1,95 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+/* macro */
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i = a; i > b; i--)
+#define int long long
+#define exist(s,e) ((s).find(e)!=(s).end())
+#define all(v) (v).begin(), (v).end()
+#define each(s,itr) for(auto (itr) = s.begin(); (itr) != s.end(); (itr)++)
+#define sum(v) accumulate(all(v), (0LL))
+#define isin(a, b, c) (b <= a && a <= c)
+#define println cout << "\n";
+#define sz(v) (int)v.size()
+
+
+/* alias */
+template<class T> using vec = vector<T>;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef tuple<int, int, int> ti;
+
+/* constant */
+const int inf = 1LL << 62;
+//const int mod = 1e9 + 7;
+const int dx[8]={1,0,-1,0,-1,1,-1,1};
+const int dy[8]={0,1,0,-1,-1,-1,1,1};
+const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+/* io_method */
+int input(){int tmp;cin >> tmp;return tmp;}
+string raw_input(){string tmp;cin >> tmp;return tmp;}
+template<class T> void printx(T n){cout << n;}
+template<class T, class U> void printx(pair<T, U> p){cout << "(" << p.first << "," << p.second << ")";}
+template<class T, class U, class V> void printx(tuple<T, U, V> t){cout << "{" << get<0>(t) << "," << get<1>(t) <<"," << get<2>(t) << "}" << endl;}
+template<class T> void printx(vector<T> v){cout << "{";rep(i,0,v.size()){printx(v[i]);if(i != v.size()-1)printx(",");}cout << "}";}
+template<class T> void print(T n){printx(n);cout << endl;}
+template<class T> void print(set<T> s){cout << "{";each(s, e){if(e != s.begin()) printx(",");printx(*e);}cout << "}" << endl;}
+template<class T, class U> void print(map<T, U> mp){cout << "{";each(mp, e){cout << "(" << e -> first << "," << e -> second << ")";}cout << "}" << endl;}
+
+
+/* general_method */
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+template<class T>T cut(T &a, int l, int r){return T(a.begin()+l, a.begin()+r);}
+
+/* main */
+int n, h;
+int a, b, c, d, e;
+void in(){
+    cin >> n >> h;
+    cin >> a >> b >> c >> d >> e;
+}
+
+void solve(){
+
+    int ans = inf;
+
+    rep(i,0,n + 1){ //i日食事をとる
+        int noteat = n - i;
+        int last = h - e * noteat;
+        if(last + b * i < 0) continue; //i日フルで普通の食事を取っても餓死
+
+        int left = 0;
+        int right = i;
+
+        rep(j,0,30){
+            int normal = (left + right) / 2; //普通の食事をとる日数
+            int poor = i - normal;
+
+            if(last + b * normal + d * poor > 0){
+                right = normal;
+            }else{
+                left = normal;
+            }
+        }
+
+        int n = right;
+        int p = i - n;
+
+        chmin(ans, a * n + c * p);
+    }
+    
+    print(ans);
+
+}
+
+signed main(){
+
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    in();
+    solve();
+    
+}
