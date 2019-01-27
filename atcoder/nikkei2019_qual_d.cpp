da7775bb71211dfe14b244f62ba3f48b3fc3284a
daleksprinter <b1015120@fun.ac.jp>
2019-01-27 22:17:05 +0900

    nikkei2019_qual_d.cpp

---
+++ atcoder/nikkei2019_qual_d.cpp
@@ -0,0 +1,119 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+/* macro */
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i = a; i > b; i--)
+#define int long long
+#define exist(s,e) ((s).find(e)!=(s).end())
+#define all(v) (v).begin(), (v).end()
+#define each(s,itr) for(auto (itr) = s.begin(); (itr) != s.end(); (itr)++)
+#define sum(v) accumulate(all(v), (0LL))
+#define isin(a, b, c) (b <= a && a <= c)
+#define println cout << "\n";
+#define sz(v) (int)v.size()
+
+
+/* alias */
+template<class T> using vec = vector<T>;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef tuple<int, int, int> ti;
+
+/* constant */
+const int inf = 1LL << 62;
+const int mod = 1e9 + 7;
+const int dx[8]={1,0,-1,0,-1,1,-1,1};
+const int dy[8]={0,1,0,-1,-1,-1,1,1};
+const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+/* io_method */
+int input(){int tmp;cin >> tmp;return tmp;}
+string raw_input(){string tmp;cin >> tmp;return tmp;}
+template<class T> void printx(T n){cout << n;}
+template<class T, class U> void printx(pair<T, U> p){cout << "(" << p.first << "," << p.second << ")";}
+template<class T, class U, class V> void printx(tuple<T, U, V> t){cout << "{" << get<0>(t) << "," << get<1>(t) <<"," << get<2>(t) << "}" << endl;}
+template<class T> void printx(vector<T> v){cout << "{";rep(i,0,v.size()){printx(v[i]);if(i != v.size()-1)printx(",");}cout << "}";}
+template<class T> void print(T n){printx(n);cout << endl;}
+template<class T> void print(set<T> s){cout << "{";each(s, e){if(e != s.begin()) printx(",");printx(*e);}cout << "}" << endl;}
+template<class T, class U> void print(map<T, U> mp){cout << "{";each(mp, e){cout << "(" << e -> first << "," << e -> second << ")";}cout << "}" << endl;}
+
+
+/* general_method */
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+template<class T>T cut(T &a, int l, int r){return T(a.begin()+l, a.begin()+r);}
+
+/* main */
+int n, m;
+vec<vi> edges(100010);
+
+void in(){
+    cin >> n >> m;
+    rep(i,0, n - 1 + m){
+        int f, t; cin >> f >> t;
+        f--; t--;
+        edges[t].push_back(f);
+    }
+
+}
+
+
+
+vi parent(100010, -1);
+vi distan(100010, -1);
+vec<bool> visited(100010, false);
+
+int dfs(int v){
+
+    if(sz(edges[v]) == 0){
+        parent[v] = -1;
+        return 0;
+    }
+
+    if(visited[v]) return distan[v];
+
+    visited[v] = true;
+
+    int par = -1;
+    int dist = -1;
+
+    rep(i,0,sz(edges[v])){
+        int nxt = edges[v][i];
+        int d = 1 + dfs(nxt);
+
+        if(dist < d){
+            par = nxt;
+            dist = d;
+        }
+    }
+    
+    parent[v] = par;
+    distan[v] = dist;
+    return dist;
+
+}
+
+
+void solve(){
+
+    rep(i,0,n){
+        dfs(i);
+    }
+
+    rep(i,0,n){
+        cout << parent[i] + 1 << (i == n - 1 ? "" : " "); 
+    }
+    cout << endl;
+
+}
+
+
+signed main(){
+
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    in();
+    solve();
+    
+}
