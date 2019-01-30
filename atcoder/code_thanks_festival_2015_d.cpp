8017206307089db78239e2624326ad0f13d71023
daleksprinter <b1015120@fun.ac.jp>
2019-01-30 10:20:29 +0900

    code_thanks_festival_2015_d.cpp

---
+++ atcoder/code_thanks_festival_2015_d.cpp
@@ -0,0 +1,98 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+/* macro */
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i = a; i > b; i--)
+#define int long long
+#define exist(s,e) ((s).find(e)!=(s).end())
+#define all(v) (v).begin(), (v).end()
+#define each(s,itr) for(auto (itr) = s.begin(); (itr) != s.end(); (itr)++)
+#define sum(v) accumulate(all(v), (0LL))
+#define isin(a, b, c) (b <= a && a <= c)
+#define println cout << "\n";
+#define sz(v) (int)v.size()
+
+
+/* alias */
+template<class T> using vec = vector<T>;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef tuple<int, int, int> ti;
+
+/* constant */
+const int inf = 1LL << 62;
+//const int mod = 1e9 + 7;
+const int dx[8]={1,0,-1,0,-1,1,-1,1};
+const int dy[8]={0,1,0,-1,-1,-1,1,1};
+const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+/* io_method */
+int input(){int tmp;cin >> tmp;return tmp;}
+string raw_input(){string tmp;cin >> tmp;return tmp;}
+template<class T> void printx(T n){cout << n;}
+template<class T, class U> void printx(pair<T, U> p){cout << "(" << p.first << "," << p.second << ")";}
+template<class T, class U, class V> void printx(tuple<T, U, V> t){cout << "{" << get<0>(t) << "," << get<1>(t) <<"," << get<2>(t) << "}" << endl;}
+template<class T> void printx(vector<T> v){cout << "{";rep(i,0,v.size()){printx(v[i]);if(i != v.size()-1)printx(",");}cout << "}";}
+template<class T> void print(T n){printx(n);cout << endl;}
+template<class T> void print(set<T> s){cout << "{";each(s, e){if(e != s.begin()) printx(",");printx(*e);}cout << "}" << endl;}
+template<class T, class U> void print(map<T, U> mp){cout << "{";each(mp, e){cout << "(" << e -> first << "," << e -> second << ")";}cout << "}" << endl;}
+
+
+/* general_method */
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+template<class T>T cut(T &a, int l, int r){return T(a.begin()+l, a.begin()+r);}
+
+/* main */
+int n;
+vi points;
+vi know;
+vi knownum;
+int acum;
+
+bool isknow[50][50];
+
+void in(){
+    cin >> n;
+    rep(i,0,n){
+        int t = input();
+        points.push_back(t);
+        know.push_back(t);
+        knownum.push_back(1);
+    }
+    acum = sum(points);
+    memset(isknow, false, sizeof(isknow));
+}
+
+void solve(){
+
+    int q = input();
+    while(q--){
+
+        int op, f, t; cin >> op >> f >> t; f--, t--;
+        if(op == 0){
+            know[f] += points[t];
+            knownum[f]++;
+            isknow[f][t] = true;
+        }else{
+
+            if(isknow[f][t]){
+                cout << points[t] << " " << points[t] << endl;
+                continue;
+            }
+            cout << max(0LL, acum - know[f] - 100 * (n - knownum[f] - 1)) << " " << min(100LL, acum - know[f]) << endl;
+        }
+
+    }
+
+}
+
+signed main(){
+
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    in();
+    solve();
+    
+}
