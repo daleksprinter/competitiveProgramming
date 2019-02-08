87fb1a533de6d835c3271883a37cc9d0366142a3
daleksprinter <b1015120@fun.ac.jp>
2019-02-08 21:27:47 +0900

    bitflyer2018_final_b.cpp

---
+++ atcoder/bitflyer2018_final_b.cpp
@@ -0,0 +1,152 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+/* macro */
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i = a; i > b; i--)
+#define int long long
+#define exist(s,e) ((s).find(e)!=(s).end())
+#define all(v) (v).begin(), (v).end()
+#define each(s,itr) for(auto (itr) = s.begin(); (itr) != s.end(); (itr)++)
+#define sum(v) accumulate(all(v), (0LL))
+#define isin(a, b, c) (b <= a && a <= c)
+#define println cout << "\n";
+#define sz(v) (int)v.size()
+
+
+/* alias */
+template<class T> using vec = vector<T>;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef tuple<int, int, int> ti;
+
+/* constant */
+const int inf = 1LL << 62;
+const int mod = 1e9 + 7;
+const int dx[8]={1,0,-1,0,-1,1,-1,1};
+const int dy[8]={0,1,0,-1,-1,-1,1,1};
+const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+/* io_method */
+int input(){int tmp;cin >> tmp;return tmp;}
+string raw_input(){string tmp;cin >> tmp;return tmp;}
+template<class T> void printx(T n){cout << n;}
+template<class T, class U> void printx(pair<T, U> p){cout << "(" << p.first << "," << p.second << ")";}
+template<class T, class U, class V> void printx(tuple<T, U, V> t){cout << "{" << get<0>(t) << "," << get<1>(t) <<"," << get<2>(t) << "}" << endl;}
+template<class T> void printx(vector<T> v){cout << "{";rep(i,0,v.size()){printx(v[i]);if(i != v.size()-1)printx(",");}cout << "}";}
+template<class T> void print(T n){printx(n);cout << endl;}
+template<class T> void print(set<T> s){cout << "{";each(s, e){if(e != s.begin()) printx(",");printx(*e);}cout << "}" << endl;}
+template<class T, class U> void print(map<T, U> mp){cout << "{";each(mp, e){cout << "(" << e -> first << "," << e -> second << ")";}cout << "}" << endl;}
+
+
+/* general_method */
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+template<class T>T cut(T &a, int l, int r){return T(a.begin()+l, a.begin()+r);}
+
+struct cumsum1d{
+    int n;
+    vector<int> arr;
+
+    cumsum1d(int len){
+        n = len;
+        rep(i,0,n) arr.push_back(0);
+    }
+
+    void init(vector<int> vec){
+        arr = vec;
+        n = arr.size();
+    }
+
+    void add(int a, int b, int val = 1){
+        arr[a] += val;
+        if(b + 1 < n) arr[b + 1] -= val;
+    }
+
+    void build(){
+        rep(i,1,n) arr[i] += arr[i-1];
+    }
+
+    int query(int from, int to){
+        if(to < from) return 0;
+        int ret = arr[to];
+        if(from) ret -= arr[from - 1];
+        return ret;
+    }
+
+};
+
+/* main */
+
+int n, q;
+vi arr;
+cumsum1d cs(10);
+
+vi c;
+vi d;
+void in(){
+    cin >> n >> q;
+    rep(i,0,n) arr.push_back(input());
+    cs.init(arr);
+    cs.build();
+    rep(i,0,q){
+        int ci, di;
+        cin >> ci >> di;
+        c.push_back(ci);
+        d.push_back(di);
+    }
+}
+
+void solve(){
+
+    rep(i,0,q){
+
+        int ans = 0;
+
+        int a = distance(arr.begin(), lower_bound(all(arr), (c[i] - d[i])));
+        ans += a * d[i];
+
+
+
+        int b = distance(arr.begin(), lower_bound(all(arr), c[i]));
+        b--;
+        ans += (b - a + 1) * c[i] - cs.query(a, b);
+
+
+
+        int e = distance(arr.begin(), lower_bound(all(arr), c[i]));
+
+        int f = distance(arr.begin(), lower_bound(all(arr), c[i] + d[i]));
+        f--;
+
+        ans += cs.query(e, f) - (f - e + 1) * c[i];
+
+
+
+        ans += (n - f - 1) * d[i];
+
+        print(ans);
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
+
+signed main(){
+
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    in();
+    solve();
+    
+}
+
