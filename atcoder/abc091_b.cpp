dc2c9031fba8c69cb980650c33f34f1038c4dc51
daleksprinter <b1015120@fun.ac.jp>
2019-02-13 21:55:41 +0900

    abc091_b.cpp

--- atcoder/abc091_b.cpp
+++ atcoder/abc091_b.cpp
@@ -1,56 +1,91 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <cstring>
-#include <math.h>
-#include <cmath>
-#include <limits.h>
-#include <map>
-#include <set>
-#include <queue>
-#include <algorithm>
-#include <functional>
-#include <stdio.h>
+#include "bits/stdc++.h"
 using namespace std;
 
+/* macro */
 #define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i = a; i > b; i--)
+#define int long long
+#define exist(s,e) ((s).find(e)!=(s).end())
+#define all(v) (v).begin(), (v).end()
+#define each(s,itr) for(auto (itr) = s.begin(); (itr) != s.end(); (itr)++)
+#define sum(v) accumulate(all(v), (0LL))
+#define isin(a, b, c) (b <= a && a <= c)
+#define println cout << "\n";
+#define sz(v) (int)v.size()
 
-const int inf = 1001001000;
-const int mod = 1000000007;
-int dx[4]={1,0,-1,0};
-int dy[4]={0,1,0,-1};
+
+/* alias */
+template<class T> using vec = vector<T>;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef tuple<int, int, int> ti;
+typedef map<int, int> mi;
+typedef set<int> si;
 
 
-int main(){
-    int n,m;
-    cin >> n;
-    map<string,int> mp;
+/* constant */
+const int inf = 1LL << 62;
+const int mod = 1e9 + 7;
+const int dx[8]={1,0,-1,0,-1,1,-1,1};
+const int dy[8]={0,1,0,-1,-1,-1,1,1};
+const string alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-    while(n--){
-        string s;
-        cin >> s;
-        if(mp.find(s) == mp.end()){
-            mp.insert(make_pair(s,1));
-        }else{
-            mp[s] = mp[s] + 1;
-        }
-    }
+/* io_method */
+int input(){int tmp;cin >> tmp;return tmp;}
+string raw_input(){string tmp;cin >> tmp;return tmp;}
+template<class T> void printx(T n){cout << n;}
+template<class T, class U> void printx(pair<T, U> p){cout << "(" << p.first << "," << p.second << ")";}
+template<class T, class U, class V> void printx(tuple<T, U, V> t){cout << "{" << get<0>(t) << "," << get<1>(t) <<"," << get<2>(t) << "}" << endl;}
+template<class T> void printx(vector<T> v){cout << "{";rep(i,0,v.size()){printx(v[i]);if(i != v.size()-1)printx(",");}cout << "}";}
+template<class T> void print(T n){printx(n);cout << endl;}
+template<class T> void print(set<T> s){cout << "{";each(s, e){if(e != s.begin()) printx(",");printx(*e);}cout << "}" << endl;}
+template<class T, class U> void print(map<T, U> mp){cout << "{";each(mp, e){cout << "(" << e -> first << "," << e -> second << ")";}cout << "}" << endl;}
+template<class T> void printans(vec<T> v){rep(i,0,sz(v)){cout << v[i] << (i == sz(v) - 1 ? "" : " ");}cout << endl;}
 
-    cin >> m;
-    while(m--){
-        string s;
-        cin >> s;
-        if(mp.find(s) != mp.end()){
-            mp[s] = mp[s] -1;
-        }
-    }
-    int ans = 0;
 
-    for(auto e: mp){
-        ans = max(ans,e.second);
-    }
-    cout << ans << endl;
+/* general_method */
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+template<class T>T cut(T &a, int l, int r){return T(a.begin()+l, a.begin()+r);}
 
+/* main */
+
+
+void in(){
     
 }
 
+void solve(){
+
+    map<string, int> mp;
+    int n; cin >> n;
+
+    while(n--){
+        string t; cin >> t;
+        mp[t]++;
+    }
+
+    int m; cin >> m;
+    while(m--){
+        string t; cin >> t;
+        mp[t]--;
+    }
+
+    int ans = 0;
+    for(auto e : mp){
+        ans = max(ans, e.second);
+    }
+
+    cout << ans << endl;
+
+
+}
+
+signed main(){
+
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    in();
+    solve();
+    
+}
