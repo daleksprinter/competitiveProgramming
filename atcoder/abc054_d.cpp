521bc6364abd2d13445e754b1944bf2198065a1f
daleksprinter <b1015120@fun.ac.jp>
2019-02-16 02:08:28 +0900

    abc054_d.cpp

--- atcoder/abc054_d.cpp
+++ atcoder/abc054_d.cpp
@@ -1,84 +1,102 @@
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
 using namespace std;
 
+/* macro */
 #define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i = a; i > b; i--)
 #define int long long
-const int inf = 100100100100100009;
-const int mod = 1000000007;
+#define exist(s,e) ((s).find(e)!=(s).end())
+#define all(v) (v).begin(), (v).end()
+#define each(s,itr) for(auto (itr) = s.begin(); (itr) != s.end(); (itr)++)
+#define sum(v) accumulate(all(v), (0LL))
+#define isin(a, b, c) (b <= a && a <= c)
+#define println cout << "\n";
+#define sz(v) (int)v.size()
 
+
+/* alias */
+template<class T> using vec = vector<T>;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef tuple<int, int, int> ti;
+typedef map<int, int> mi;
+typedef set<int> si;
+
+
+/* constant */
+const int inf = 1LL << 62;
+const int mod = 1e9 + 7;
+const int dx[8]={1,0,-1,0,-1,1,-1,1};
+const int dy[8]={0,1,0,-1,-1,-1,1,1};
+const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+/* io_method */
+int input(){int tmp;cin >> tmp;return tmp;}
+string raw_input(){string tmp;cin >> tmp;return tmp;}
+template<class T> void printx(T n){cout << n;}
+template<class T, class U> void printx(pair<T, U> p){cout << "(" << p.first << "," << p.second << ")";}
+template<class T, class U, class V> void printx(tuple<T, U, V> t){cout << "{" << get<0>(t) << "," << get<1>(t) <<"," << get<2>(t) << "}" << endl;}
+template<class T> void printx(vector<T> v){cout << "{";rep(i,0,v.size()){printx(v[i]);if(i != v.size()-1)printx(",");}cout << "}";}
+template<class T> void print(T n){printx(n);cout << endl;}
+template<class T> void print(set<T> s){cout << "{";each(s, e){if(e != s.begin()) printx(",");printx(*e);}cout << "}" << endl;}
+template<class T, class U> void print(map<T, U> mp){cout << "{";each(mp, e){cout << "(" << e -> first << "," << e -> second << ")";}cout << "}" << endl;}
+template<class T> void printans(vec<T> v){rep(i,0,sz(v)){cout << v[i] << (i == sz(v) - 1 ? "" : " ");}cout << endl;}
+
+
+/* general_method */
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+template<class T>T cut(T &a, int l, int r){return T(a.begin()+l, a.begin()+r);}
+
+/* main */
+
+int n, ma, mb;
+vec<ti> arr;
+void in(){
+    cin >> n >> ma >> mb;
+    rep(i,0,n){
+        int a, b, c;
+        cin >> a >> b >> c;
+        arr.push_back(ti(a, b, c));
+    }
+}
+int dp[40][410][410];
+
+bool isok(int a, int b){
+    return (a > 0 && b > 0 && b * ma == mb * a);
+}
+
+int dfs(int i, int suma, int sumb){
+
+    if(i == n) return (isok(suma, sumb) ? 0 : inf);
+
+    if(dp[i][suma][sumb] > -1) return dp[i][suma][sumb];
+
+    int ans = (isok(suma, sumb) ? 0 : inf);
+
+    chmin(ans, dfs(i + 1, suma, sumb));
+
+    int a = get<0>(arr[i]), b = get<1>(arr[i]), c = get<2>(arr[i]);
+
+    chmin(ans, c + dfs(i + 1, suma + a, sumb + b));
+
+    return dp[i][suma][sumb] = ans;
+
+}
+
+void solve(){
+
+    memset(dp, -1, sizeof(dp));
+    int ans = dfs(0, 0, 0);
+    print(ans == inf ? -1 : ans);
+
+}
 
 signed main(){
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(0);
 
-    int n, ma, mb; cin >> n >> ma >> mb;
-
-    vector<vector<int> > arrf;
-    vector<vector<int> > arrs;
-    rep(i,0,n/2){
-        int a, b, cost;
-        cin >> a >> b >> cost;
-        vector<int> tmp = {a, b, cost};
-        arrf.push_back(tmp); 
-    }
-
-    rep(i,n/2,n){
-        int a, b, cost;
-        cin >> a >> b >> cost;
-        vector<int> tmp = {a, b, cost};
-        arrs.push_back(tmp); 
-    }
-
-    vector< vector<int> > fir;
-    rep(i,0,pow(2,arrf.size())){
-        int asum = 0, bsum = 0, csum = 0;
-        rep(j,0,arrf.size()){
-            if(i & (1 << j)){
-                asum += arrf[j][0]; bsum += arrf[j][1]; csum += arrf[j][2];
-            }
-        }
-        vector<int> tmp = {asum, bsum, csum}; fir.push_back(tmp);
-    }
-
-    map<pair<int, int> , int> mp;
-    rep(i,0,pow(2,arrs.size())){
-        int asum = 0, bsum = 0, csum = 0;
-        rep(j,0,arrs.size()){
-            if(i & (1 << j)){
-                asum += arrs[j][0]; bsum += arrs[j][1]; csum += arrs[j][2];
-            }
-        }
-        pair<int, int> key = make_pair(asum, bsum); 
-        
-        if(mp.find(key) == mp.end()){ //not found
-            mp[key] = csum;
-        }else{
-            mp[key] = min(mp[key], csum);
-        }
-    }
-
-    int ans = inf;
-    rep(i,0, fir.size()){
-        rep(j,1,15){
-            pair<int, int> key = make_pair(ma * j - fir[i][0], mb * j - fir[i][1]);
-            if(mp.find(key) == mp.end()){
-                continue;
-            }else{
-                ans = min(ans, fir[i][2] + mp[key]);
-            }
-        }
-    }
-
-    if(ans == inf){
-        cout << -1 << endl;
-    }else{
-        cout << ans << endl;
-    }
-    return 0;
-
-
-
-
-
-
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    in();
+    solve();
+    
 }
