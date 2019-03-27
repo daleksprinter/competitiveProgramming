92eed614c5b5fe49e031a1315804f825ae9af729
daleksprinter <b1015120@fun.ac.jp>
2019-03-27 15:21:20 +0900

    joi2013yo_d.cpp

---
+++ atcoder/joi2013yo_d.cpp
@@ -0,0 +1,107 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+/* macro */
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i = a; i > b; i--)
+#define int long long
+#define exist(s,e) ((s).find(e)!=(s).end())
+#define all(v) (v).begin(), (v).end()
+#define each(s,itr) for(auto (itr) = s.begin(); (itr) != s.end(); (itr)++)
+#define sum(v) accumulate(all(v), (0LL))
+#define isin(a, b, c) (b <= a && a <= c)
+#define println cout << "\n";
+#define sz(v) (int)v.size()
+#define bin(x) static_cast<bitset<16> >(x)
+
+
+/* alias */
+template<class T> using vec = vector<T>;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef tuple<int, int, int> ti;
+typedef map<int, int> mi;
+typedef set<int> si;
+
+
+/* constant */
+const int inf = 1LL << 62;
+const int mod = 1e9 + 7;
+const int dx[8]={1,0,-1,0,-1,1,-1,1};
+const int dy[8]={0,1,0,-1,-1,-1,1,1};
+const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+/* io_method */
+int input(){int tmp;cin >> tmp;return tmp;}
+string raw_input(){string tmp;cin >> tmp;return tmp;}
+template<class T> void printx(T n){cout << n;}
+template<class T, class U> void printx(pair<T, U> p){cout << "(" << p.first << "," << p.second << ")";}
+template<class T, class U, class V> void printx(tuple<T, U, V> t){cout << "{" << get<0>(t) << "," << get<1>(t) <<"," << get<2>(t) << "}" << endl;}
+template<class T> void printx(vector<T> v){cout << "{";rep(i,0,v.size()){printx(v[i]);if(i != v.size()-1)printx(",");}cout << "}";}
+template<class T> void print(T n){printx(n);cout << endl;}
+template<class T> void print(set<T> s){cout << "{";each(s, e){if(e != s.begin()) printx(",");printx(*e);}cout << "}" << endl;}
+template<class T, class U> void print(map<T, U> mp){cout << "{";each(mp, e){cout << "(" << e -> first << "," << e -> second << ")";}cout << "}" << endl;}
+template<class T> void printans(vec<T> v){rep(i,0,sz(v)){cout << v[i] << (i == sz(v) - 1 ? "" : " ");}cout << endl;}
+
+
+/* general_method */
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+template<class T>T cut(T &a, int l, int r){return T(a.begin()+l, a.begin()+r);}
+
+
+/* math_method */
+int ceil(int a, int b){return a / b + (a % b > 0);}
+
+
+/* main */
+
+int d, n;
+vi temp;
+vi f;
+vi t;
+vi c;
+void in(){
+    cin >> d >> n;
+    rep(i,0,d) temp.push_back(input());
+    rep(i,0,n){
+        int a, b, ci;
+        cin >> a >> b >> ci;
+        f.push_back(a);
+        t.push_back(b);
+        c.push_back(ci);
+    }
+}
+int dp[210][110];
+int dfs(int i, int prev){
+
+    if(i == d) return 0;
+    if(dp[i][prev] > -1) return dp[i][prev];
+    int ans = 0;
+    rep(j,0,n){
+        if(!isin(temp[i], f[j], t[j])) continue;
+        chmax(ans, abs(prev - c[j]) + dfs(i + 1, c[j]));
+    }
+
+    return dp[i][prev] = ans;
+}
+
+void solve(){
+    memset(dp, -1, sizeof(dp));
+    int ans = 0;
+    rep(i,0,n){
+        if(!isin(temp[0], f[i], t[i])) continue;
+        chmax(ans, dfs(1, c[i]));
+    }
+
+    print(ans);
+}
+
+signed main(){
+
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    in();
+    solve();
+    
+}
