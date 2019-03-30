2ca83fc82bed5d486d32c9cf3f161ada48453ef0
daleksprinter <b1015120@fun.ac.jp>
2019-03-31 01:59:09 +0900

    code_thanks_festival_2017_f.cpp

---
+++ atcoder/code_thanks_festival_2017_f.cpp
@@ -0,0 +1,106 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+/* macro */
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i = a; i > b; i--)
+#define int long long
+#define exist(s,e) ((s).find(e)!=(s).end())
+#define all(v) (v).begin(), (v).end()
+#define each(s,itr) for(auto (itr) = s.begin(); (itr) != s.end(); (itr)++)
+#define sum(v) accumulate(all(v), (0LL))
+#define isin(a, b, c) (b <= a && a <= c)
+#define println cout << "\n";
+#define sz(v) (int)v.size()
+#define bin(x) static_cast<bitset<16> >(x)
+
+
+/* alias */
+template<class T> using vec = vector<T>;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef tuple<int, int, int> ti;
+typedef map<int, int> mi;
+typedef set<int> si;
+
+
+/* constant */
+const int inf = 1LL << 62;
+const int mod = 1e9 + 7;
+const int dx[8]={1,0,-1,0,-1,1,-1,1};
+const int dy[8]={0,1,0,-1,-1,-1,1,1};
+const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+/* io_method */
+int input(){int tmp;cin >> tmp;return tmp;}
+string raw_input(){string tmp;cin >> tmp;return tmp;}
+template<class T> void printx(T n){cout << n;}
+template<class T, class U> void printx(pair<T, U> p){cout << "(" << p.first << "," << p.second << ")";}
+template<class T, class U, class V> void printx(tuple<T, U, V> t){cout << "{" << get<0>(t) << "," << get<1>(t) <<"," << get<2>(t) << "}" << endl;}
+template<class T> void printx(vector<T> v){cout << "{";rep(i,0,v.size()){printx(v[i]);if(i != v.size()-1)printx(",");}cout << "}";}
+template<class T> void print(T n){printx(n);cout << endl;}
+template<class T> void print(set<T> s){cout << "{";each(s, e){if(e != s.begin()) printx(",");printx(*e);}cout << "}" << endl;}
+template<class T, class U> void print(map<T, U> mp){cout << "{";each(mp, e){cout << "(" << e -> first << "," << e -> second << ")";}cout << "}" << endl;}
+template<class T> void printans(vec<T> v){rep(i,0,sz(v)){cout << v[i] << (i == sz(v) - 1 ? "" : " ");}cout << endl;}
+
+
+/* general_method */
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+template<class T>T cut(T &a, int l, int r){return T(a.begin()+l, a.begin()+r);}
+
+
+/* math_method */
+int ceil(int a, int b){return a / b + (a % b > 0);}
+
+
+/* main */
+
+int n, k;
+vi arr(101010, 0);
+vi b;
+vi c;
+vi pow2 = {1};
+void in(){
+    cin >> n >> k;
+    rep(i,0,n) arr[input()]++;
+    rep(i,0,101010){
+        if(arr[i] > 0){
+            b.push_back(i);
+            c.push_back(arr[i]);
+        }
+    }
+    rep(i,0,101010) pow2.push_back(pow2.back() * 2 % mod); 
+}
+
+
+vi dp1(201010, 0);
+vi dp2(201010, 0);
+
+void solve(){
+
+    dp1[0] = 1;
+    rep(i,0,sz(b)){
+        rep(j,0,201010){
+            if(dp1[j] == 0) continue;
+            dp2[j] += dp1[j] * pow2[c[i] - 1] % mod;
+            dp2[j] %= mod;
+            dp2[j ^ b[i]] += dp1[j] * pow2[c[i] - 1] % mod;
+            dp2[j ^ b[i]] %= mod;
+           
+        }
+        swap(dp1, dp2);
+        dp2 = vi(201010, 0);
+    }
+    print(dp1[k]);
+
+}
+
+signed main(){
+
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    in();
+    solve();
+    
+}
