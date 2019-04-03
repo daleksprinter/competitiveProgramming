6ec2dcbce1da99b8cd855d2e198ba66c2b84a3c0
daleksprinter <b1015120@fun.ac.jp>
2019-04-03 15:54:23 +0900

    joi2009yo_b.cpp

--- atcoder/joi2009yo_b.cpp
+++ atcoder/joi2009yo_b.cpp
@@ -1,7 +1,6 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-
 /* macro */
 #define rep(i,a,b) for(int i=a;i<b;i++)
 #define revrep(i,a,b) for(int i = a; i > b; i--)
@@ -10,98 +9,84 @@ using namespace std;
 #define all(v) (v).begin(), (v).end()
 #define each(s,itr) for(auto (itr) = s.begin(); (itr) != s.end(); (itr)++)
 #define sum(v) accumulate(all(v), (0LL))
-#define pb push_back
+#define isin(a, b, c) (b <= a && a <= c)
+#define println cout << "\n";
+#define sz(v) (int)v.size()
+#define bin(x) static_cast<bitset<16> >(x)
 
 
 /* alias */
 template<class T> using vec = vector<T>;
 typedef vector<int> vi;
 typedef pair<int, int> pi;
+typedef tuple<int, int, int> ti;
+typedef map<int, int> mi;
+typedef set<int> si;
 
 
 /* constant */
-const int inf = pow(2, 62);
+const int inf = 1LL << 62;
 const int mod = 1e9 + 7;
-int dx[8]={1,0,-1,0,-1,1,-1,1};
-int dy[8]={0,1,0,-1,-1,-1,1,1};
-const string alpha = "abcdefghijklmnopqrstuvwxyz";
+const int dx[8]={1,0,-1,0,-1,1,-1,1};
+const int dy[8]={0,1,0,-1,-1,-1,1,1};
+const string alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 /* io_method */
 int input(){int tmp;cin >> tmp;return tmp;}
 string raw_input(){string tmp;cin >> tmp;return tmp;}
-string readline(){string s;getline(cin, s);return s;}
 template<class T> void printx(T n){cout << n;}
 template<class T, class U> void printx(pair<T, U> p){cout << "(" << p.first << "," << p.second << ")";}
+template<class T, class U, class V> void printx(tuple<T, U, V> t){cout << "{" << get<0>(t) << "," << get<1>(t) <<"," << get<2>(t) << "}" << endl;}
 template<class T> void printx(vector<T> v){cout << "{";rep(i,0,v.size()){printx(v[i]);if(i != v.size()-1)printx(",");}cout << "}";}
 template<class T> void print(T n){printx(n);cout << endl;}
 template<class T> void print(set<T> s){cout << "{";each(s, e){if(e != s.begin()) printx(",");printx(*e);}cout << "}" << endl;}
 template<class T, class U> void print(map<T, U> mp){cout << "{";each(mp, e){cout << "(" << e -> first << "," << e -> second << ")";}cout << "}" << endl;}
-template<class T> void printans(vector<T> v){
-    rep(i,0,v.size() - 1){
-        cout << v[i] + 1 << " ";
-    }
-    cout << v[v.size() - 1] + 1 << endl;
-}
+template<class T> void printans(vec<T> v){rep(i,0,sz(v)){cout << v[i] << (i == sz(v) - 1 ? "" : " ");}cout << endl;}
+
 
 /* general_method */
 template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
 template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+template<class T>T cut(T &a, int l, int r){return T(a.begin()+l, a.begin()+r);}
 
 
+/* math_method */
+int ceil(int a, int b){return a / b + (a % b > 0);}
 
 
 /* main */
 
+vi a;
+vi b;
+void in(){
+    rep(i,0,10)a.push_back(input());
+    rep(i,0,10) b.push_back(input());
+    sort(all(a));
+    reverse(all(a));
+    sort(all(b));
+    reverse(all(b));
+}
+
+void solve(){
+
+    int ans = 0;
+    vi tmp = cut(a,0,3);
+    ans = sum(tmp);
+
+
+    int hoge = 0;
+    tmp = cut(b,0,3);
+    hoge = sum(tmp);
+    cout << ans << " " << hoge << endl;
+
+
+}
 
 signed main(){
 
     cin.tie(0);
     ios::sync_with_stdio(false);
-
-    vi a;
-    rep(i,0,10){
-        a.push_back(input());
-    }
-
-    sort(all(a));
-    reverse(all(a));
-
-    cout << a[0] + a[1] + a[2] << " ";
-
-    vi b;
-    rep(i,0,10){
-        b.push_back(input());
-    }
-
-    sort(all(b));
-    reverse(all(b));
-
-    cout << b[0] + b[1] + b[2] << endl;
-
-
-
-
-
-
+    in();
+    solve();
     
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-    
-
 }
-
-
