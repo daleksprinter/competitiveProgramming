67463a03e272fba2349eda82b5be0ca738e566be
daleksprinter <b1015120@fun.ac.jp>
2019-04-03 15:50:53 +0900

    joi2009yo_a.cpp

--- atcoder/joi2009yo_a.cpp
+++ atcoder/joi2009yo_a.cpp
@@ -1,7 +1,6 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-
 /* macro */
 #define rep(i,a,b) for(int i=a;i<b;i++)
 #define revrep(i,a,b) for(int i = a; i > b; i--)
@@ -10,92 +9,83 @@ using namespace std;
 #define all(v) (v).begin(), (v).end()
 #define each(s,itr) for(auto (itr) = s.begin(); (itr) != s.end(); (itr)++)
 #define sum(v) accumulate(all(v), (0LL))
-#define pb push_back
+#define isin(a, b, c) (b <= a && a <= c)
+#define println cout << "\n";
+#define sz(v) (int)v.size()
+#define bin(x) static_cast<bitset<16> >(x)
 
 
 /* alias */
 template<class T> using vec = vector<T>;
 typedef vector<int> vi;
 typedef pair<int, int> pi;
+typedef tuple<int, int, int> ti;
+typedef map<int, int> mi;
+typedef set<int> si;
 
 
 /* constant */
-const int inf = pow(2, 62);
+const int inf = 1LL << 62;
 const int mod = 1e9 + 7;
-int dx[8]={1,0,-1,0,-1,1,-1,1};
-int dy[8]={0,1,0,-1,-1,-1,1,1};
-const string alpha = "abcdefghijklmnopqrstuvwxyz";
+const int dx[8]={1,0,-1,0,-1,1,-1,1};
+const int dy[8]={0,1,0,-1,-1,-1,1,1};
+const string alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 /* io_method */
 int input(){int tmp;cin >> tmp;return tmp;}
 string raw_input(){string tmp;cin >> tmp;return tmp;}
-string readline(){string s;getline(cin, s);return s;}
 template<class T> void printx(T n){cout << n;}
 template<class T, class U> void printx(pair<T, U> p){cout << "(" << p.first << "," << p.second << ")";}
+template<class T, class U, class V> void printx(tuple<T, U, V> t){cout << "{" << get<0>(t) << "," << get<1>(t) <<"," << get<2>(t) << "}" << endl;}
 template<class T> void printx(vector<T> v){cout << "{";rep(i,0,v.size()){printx(v[i]);if(i != v.size()-1)printx(",");}cout << "}";}
 template<class T> void print(T n){printx(n);cout << endl;}
 template<class T> void print(set<T> s){cout << "{";each(s, e){if(e != s.begin()) printx(",");printx(*e);}cout << "}" << endl;}
 template<class T, class U> void print(map<T, U> mp){cout << "{";each(mp, e){cout << "(" << e -> first << "," << e -> second << ")";}cout << "}" << endl;}
-template<class T> void printans(vector<T> v){
-    rep(i,0,v.size() - 1){
-        cout << v[i] + 1 << " ";
-    }
-    cout << v[v.size() - 1] + 1 << endl;
-}
+template<class T> void printans(vec<T> v){rep(i,0,sz(v)){cout << v[i] << (i == sz(v) - 1 ? "" : " ");}cout << endl;}
+
 
 /* general_method */
 template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
 template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+template<class T>T cut(T &a, int l, int r){return T(a.begin()+l, a.begin()+r);}
 
 
+/* math_method */
+int ceil(int a, int b){return a / b + (a % b > 0);}
 
 
 /* main */
 
 
+void in(){
+
+}
+
+void solve(){
+
+    rep(i,0,3){
+        int a, b, c, d, e, f;
+        cin >> a >> b >> c >> d >> e >> f;
+        int go = a * 60 * 60 + b * 60 + c;
+        int back = d * 60 * 60 + e * 60 + f;
+        int ans = back - go;
+
+        cout << ans / (60 * 60) << " ";
+        ans %= 60 * 60;
+
+        cout << ans / 60 << " ";
+        ans %= 60;
+
+        cout << ans % 60 << endl;
+    }
+
+}
+
 signed main(){
 
     cin.tie(0);
     ios::sync_with_stdio(false);
-
-    rep(i,0,3){
-        int sh, sm, ss, gh, gm, gs;
-        cin >> sh >> sm >> ss >> gh >> gm >> gs;
-
-        int cnt = gh * 60 * 60 + gm * 60 + gs - sh * 60 * 60 - sm * 60 - ss;
-
-        int h = cnt/(60 * 60);
-        int m = cnt % (60 * 60) / 60;
-        int s = cnt % 60;
-
-        cout << h << " " << m << " " << s << endl;
-    }
-
-
-
-
-
-
-
+    in();
+    solve();
     
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-    
-
 }
-
-
