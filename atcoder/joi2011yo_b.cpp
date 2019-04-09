47b7a793a02caa58d6306eec67e80fb08e997ee5
daleksprinter <b1015120@fun.ac.jp>
2019-04-09 19:58:36 +0900

    joi2011yo_b.cpp

--- atcoder/joi2011yo_b.cpp
+++ atcoder/joi2011yo_b.cpp
@@ -9,122 +9,86 @@ using namespace std;
 #define all(v) (v).begin(), (v).end()
 #define each(s,itr) for(auto (itr) = s.begin(); (itr) != s.end(); (itr)++)
 #define sum(v) accumulate(all(v), (0LL))
-#define pb push_back
+#define isin(a, b, c) (b <= a && a <= c)
+#define println cout << "\n";
+#define sz(v) (int)v.size()
+#define bin(x) static_cast<bitset<16> >(x)
 
 
 /* alias */
-
-
 template<class T> using vec = vector<T>;
 typedef vector<int> vi;
 typedef pair<int, int> pi;
+typedef tuple<int, int, int> ti;
+typedef map<int, int> mi;
+typedef set<int> si;
 
 
 /* constant */
-const int inf = 1000100010010010;
+const int inf = 1LL << 62;
 const int mod = 1e9 + 7;
-int dx[8]={1,0,-1,0,-1,1,-1,1};
-int dy[8]={0,1,0,-1,-1,-1,1,1};
-
+const int dx[8]={1,0,-1,0,-1,1,-1,1};
+const int dy[8]={0,1,0,-1,-1,-1,1,1};
+const string alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 /* io_method */
 int input(){int tmp;cin >> tmp;return tmp;}
 string raw_input(){string tmp;cin >> tmp;return tmp;}
-string readline(){string s;getline(cin, s);return s;}
 template<class T> void printx(T n){cout << n;}
 template<class T, class U> void printx(pair<T, U> p){cout << "(" << p.first << "," << p.second << ")";}
+template<class T, class U, class V> void printx(tuple<T, U, V> t){cout << "{" << get<0>(t) << "," << get<1>(t) <<"," << get<2>(t) << "}" << endl;}
 template<class T> void printx(vector<T> v){cout << "{";rep(i,0,v.size()){printx(v[i]);if(i != v.size()-1)printx(",");}cout << "}";}
 template<class T> void print(T n){printx(n);cout << endl;}
 template<class T> void print(set<T> s){cout << "{";each(s, e){if(e != s.begin()) printx(",");printx(*e);}cout << "}" << endl;}
 template<class T, class U> void print(map<T, U> mp){cout << "{";each(mp, e){cout << "(" << e -> first << "," << e -> second << ")";}cout << "}" << endl;}
+template<class T> void printans(vec<T> v){rep(i,0,sz(v)){cout << v[i] << (i == sz(v) - 1 ? "" : " ");}cout << endl;}
 
 
 /* general_method */
 template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
 template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+template<class T>T cut(T &a, int l, int r){return T(a.begin()+l, a.begin()+r);}
+
+
+/* math_method */
+int ceil(int a, int b){return a / b + (a % b > 0);}
 
 
 /* main */
-template<typename T>
-auto cut(T &a, int l, int r){
-    //get a[l:r]
-    return T(a.begin()+l, a.begin()+r);   
-}
 
+string t;
+int n;
+vec<string>arr;
+
+void in(){
+    cin >> t >> n;
+    rep(i,0,n)arr.push_back(raw_input());
+ }
+
+void solve(){
+
+    int ans = 0;
+    rep(i,0,n){
+        bool flag = false;
+        rep(start,0,20){
+            bool isok = true;
+            rep(j,0,sz(t)){
+                if(arr[i][(start + j) % 10] != t[j]) isok = false;
+            }
+            if(isok) flag = true;
+        }
+        if(flag) ans++;
+    }
+
+    print(ans);
+
+}
 
 signed main(){
 
-
     cin.tie(0);
     ios::sync_with_stdio(false);
-
-
-	string s = raw_input();
-
-	int n = input();
-
-	int ans = 0;
-
-	rep(i,0,n){
-		string t = raw_input();
-		t += t;
-
-		rep(j,0,t.size()){
-			if(s == cut(t, j, j + s.size())){
-				ans++;
-				break;
-			}
-		}
-	}
-
-	print(ans);
-
-	
-
-
-
-	
-	
-
-
-	
-
-
-
-
-	
-
-
-
-
+    in();
+    solve();
     
-
-
-
-	
-
-
-
-
-
-
-
-
-    
-
-
-
-    
-
-
-
-
-
-    
-
-
-    
-
 }
-
-
