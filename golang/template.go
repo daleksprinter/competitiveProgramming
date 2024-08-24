d5f6453878b8d727006c1bc57cf161513e6b32c6
r-sekine <ca01376@cartahd.com>
2024-08-24 16:37:45 +0900

    fix

--- golang/template.go
+++ golang/template.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func input() int {
 	var tmp int
@@ -8,8 +14,46 @@ func input() int {
 	return tmp
 }
 
+var (
+	sc = bufio.NewScanner(os.Stdin)
+)
+
+func input_arr() []int {
+	sc.Scan()
+	strs := strings.Split(sc.Text(), " ")
+	nums := make([]int, len(strs))
+	for i, s := range strs {
+		nums[i], _ = strconv.Atoi(s)
+	}
+	return nums
+}
+
+func print_arr(arr []int) {
+	for _, v := range arr {
+		fmt.Print(v, " ")
+	}
+	fmt.Println()
+}
+
+func print_arr2(arr [][]int) {
+	for _, v := range arr {
+		for _, vv := range v {
+			fmt.Print(vv, " ")
+		}
+		fmt.Println()
+	}
+}
+
+func output_arr(arr []int) {
+	for i, v := range arr {
+		if i != 0 {
+			fmt.Print(" ")
+		}
+		fmt.Print(v)
+	}
+	fmt.Println()
+}
+
 func main() {
-	i := input()
-	fmt.Println(i)
-	return
+
 }
